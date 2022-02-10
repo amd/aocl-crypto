f8ba2c3f8cb64ce1a2dc4407255e62f0337d0294
Phani Shankar Madineni <PhaniShankar.Madineni@amd.com>
2022-02-10 18:11:36 +0000

    crypto: digest: sha512 is integrated
    Signed-off-by: Phani Shankar Madineni <PhaniShankar.Madineni@amd.com>

--- lib/digest/builder.cc
+++ lib/digest/builder.cc
@@ -138,6 +138,10 @@ class Sha2Builder
                 __build_sha<Sha224>(rDigestInfo, rCtx);
                 break;
 
+            case ALC_DIGEST_LEN_512:
+                __build_sha<Sha512>(rDigestInfo, rCtx);
+                break;
+
             default:
                 Error::setGeneric(err, ALC_ERROR_NOT_SUPPORTED);
                 break;
