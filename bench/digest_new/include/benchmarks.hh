0c445fb26e5e3389f08df937131d6bf2d6faf897
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2022-03-17 11:29:45 +0530

    Crypto:Bench:Digest:Updated SHA2_224 benchmarks with diff block sizes provided

--- bench/digest_new/include/benchmarks.hh
+++ bench/digest_new/include/benchmarks.hh
@@ -9,33 +9,62 @@
 
 using namespace alcp::bench;
 
-/* add all your new benchmark tests here */
-static void
-BENCHMARK_SHA_2_224(benchmark::State& state) {
+void
+Digest_SHA2_224(benchmark::State& state, uint64_t block_size) {
     alc_error_t error;
     alc_digest_handle_t handle;
     /*update this*/
-    DataSet ds = DataSet("/home/pjayaraj/aocl-crypto/bench/digest_new/test_data/dataset_SHA_224.csv");
-
-    uint64_t input_size;
-    while (ds.readMsgDigest()) {
-        for (auto _ : state) { 
-            AlcpDigestBase DigestBase(&handle, ALC_SHA2_224, ALC_DIGEST_TYPE_SHA2, ALC_DIGEST_LEN_224);
-            uint8_t * message = &(ds.getMessage()[0]);
-            //uint8_t * expected = &(ds.getDigest()[0]);
-            uint8_t digest[512] = { 0 };
-            input_size = ds.getMessage().size();
-            error = DigestBase.digest_function(&handle, message, ds.getMessage().size(), digest, sizeof(digest));
-            if (alcp_is_error(error)) {
-                printf("Error");
-            }
-            alcp_digest_finish(&handle);
-            //check op and expected
+    uint8_t * message = (uint8_t*)malloc(16384);
+    for (auto _ : state) {
+        AlcpDigestBase DigestBase(&handle, ALC_SHA2_224, ALC_DIGEST_TYPE_SHA2, ALC_DIGEST_LEN_224);
+        uint8_t digest[512] = { 0 };
+        error = DigestBase.digest_function(&handle, message, block_size, digest, sizeof(digest));
+        if (alcp_is_error(error)) {
+            printf("Error");
+            return;
         }
-        state.counters["Speed(Bits/Sec)"] = benchmark::Counter(state.iterations() * input_size * 8,
-                                                    benchmark::Counter::kIsRate);
-        state.counters["InputSize(Bytes)"] = input_size;
+        alcp_digest_finish(&handle);
     }
+    state.counters["Speed(Bits/s)"] = benchmark::Counter(state.iterations() * block_size * 8,
+                                                        benchmark::Counter::kIsRate);
+    state.counters["BlockSize(Bytes)"] = block_size;
+    free(message);
     return;
 }
-BENCHMARK(BENCHMARK_SHA_2_224);
+
+/* add all your new benchmarks here */
+static void
+BENCH_SHA2_224_16(benchmark::State& state) {
+    Digest_SHA2_224(state, 16);
+}
+BENCHMARK(BENCH_SHA2_224_16);
+
+static void
+BENCH_SHA2_224_64(benchmark::State& state) {
+    Digest_SHA2_224(state, 64);
+}
+BENCHMARK(BENCH_SHA2_224_64);
+
+static void
+BENCH_SHA2_224_256(benchmark::State& state) {
+    Digest_SHA2_224(state, 256);
+}
+BENCHMARK(BENCH_SHA2_224_256);
+
+static void
+BENCH_SHA2_224_1024(benchmark::State& state) {
+    Digest_SHA2_224(state, 1024);
+}
+BENCHMARK(BENCH_SHA2_224_1024);
+
+static void
+BENCH_SHA2_224_8192(benchmark::State& state) {
+    Digest_SHA2_224(state, 8192);
+}
+BENCHMARK(BENCH_SHA2_224_8192);
+
+static void
+BENCH_SHA2_224_16384(benchmark::State& state) {
+    Digest_SHA2_224(state, 16384);
+}
+BENCHMARK(BENCH_SHA2_224_16384);
