cfa04914b5af0868f4e22558dfc61d7f9a098c5b
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2022-03-31 14:04:14 +0530

    Crypto:Digest:KAT Tests: Updated with testing OpenSSL Schemes

--- tests/digest/test_main.cc
+++ tests/digest/test_main.cc
@@ -94,6 +94,12 @@ TEST(DIGEST_SHA2, KAT_256)
     if (useipp == true)
         db = &idb;
 #endif
+
+#ifdef USE_OSSL
+    OpenSSLDigestBase odb(ALC_SHA2_256, ALC_DIGEST_TYPE_SHA2, ALC_DIGEST_LEN_256);
+    if (useossl == true)
+        db = &odb;
+#endif
     while (ds.readMsgDigest()) {
         error = db->digest_function(&(ds.getMessage()[0]),
                                     ds.getMessage().size(),
@@ -126,6 +132,13 @@ TEST(DIGEST_SHA2, KAT_384)
     if (useipp == true)
         db = &idb;
 #endif
+
+#ifdef USE_OSSL
+    OpenSSLDigestBase odb(ALC_SHA2_384, ALC_DIGEST_TYPE_SHA2, ALC_DIGEST_LEN_384);
+    if (useossl == true)
+        db = &odb;
+#endif
+
     while (ds.readMsgDigest()) {
         error = db->digest_function(&(ds.getMessage()[0]),
                                     ds.getMessage().size(),
@@ -158,6 +171,13 @@ TEST(DIGEST_SHA2, KAT_512)
     if (useipp == true)
         db = &idb;
 #endif
+
+#ifdef USE_OSSL
+    OpenSSLDigestBase odb(ALC_SHA2_512, ALC_DIGEST_TYPE_SHA2, ALC_DIGEST_LEN_512);
+    if (useossl == true)
+        db = &odb;
+#endif
+
     while (ds.readMsgDigest()) {
         error = db->digest_function(&(ds.getMessage()[0]),
                                     ds.getMessage().size(),
