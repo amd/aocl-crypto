6adffe5d50f2c9c010f2d23e06d7c4be7b67abfc
Abhiram S <abhiram.s@amd.com>
2022-06-03 23:06:38 +0530

    Cipher:AES-CTR: VAES 8 block unrolling
    Performance before this commit was slightly better than aesni, but after
    this commit, it should be on par with aes-cfb's result.
    
    Signed-off-by: Abhiram S <abhiram.s@amd.com>

--- lib/arch/zen3/vaes_ctr.cc
+++ lib/arch/zen3/vaes_ctr.cc
@@ -63,9 +63,9 @@ cryptCtr(const uint8_t* pPlainText,  // ptr to plaintext
     auto p_out_128 = reinterpret_cast<__m128i*>(pCipherText);
     auto pkey128   = reinterpret_cast<const __m128i*>(pKey);
 
-    __m256i    block0, block1;
-    vect_256_t ctr1, ctr2;
-    __m256i    b1, b2, swap_ctrx;
+    __m256i    block0, block1, block2, block3;
+    vect_256_t ctr1, ctr2, ctr3, ctr4;
+    __m256i    b1, b2, b3, b4, swap_ctrx;
 
     //
     // counterblock :: counter 4 bytes: IV 8 bytes : Nonce 4 bytes
@@ -95,6 +95,8 @@ cryptCtr(const uint8_t* pPlainText,  // ptr to plaintext
     __m256i onehi    = _mm256_setr_epi32(0, 0, 0, 0, 0, 0, 0, 1);
     __m256i twohilo  = _mm256_setr_epi32(0, 0, 0, 2, 0, 0, 0, 2);
     __m256i fourhilo = _mm256_setr_epi32(0, 0, 0, 4, 0, 0, 0, 4);
+    // __m256i sixhilo   = _mm256_setr_epi32(0, 0, 0, 6, 0, 0, 0, 6);
+    __m256i eighthilo = _mm256_setr_epi32(0, 0, 0, 8, 0, 0, 0, 8);
 
     // Rearrange to add
     ctr1.ymm = _mm256_shuffle_epi8(ctr1.ymm, swap_ctrx);
@@ -102,6 +104,41 @@ cryptCtr(const uint8_t* pPlainText,  // ptr to plaintext
     // Keep both counters ready
     ctr1.ymm = _mm256_add_epi32(ctr1.ymm, onehi);
     ctr2.ymm = _mm256_add_epi32(ctr1.ymm, twohilo);
+    ctr3.ymm = _mm256_add_epi32(ctr2.ymm, twohilo);
+    ctr4.ymm = _mm256_add_epi32(ctr3.ymm, twohilo);
+
+    for (; blocks >= 8; blocks -= 8) {
+        block0 = _mm256_loadu_si256((__m256i*)p_in_128);
+        block1 = _mm256_loadu_si256((__m256i*)p_in_128 + 1);
+        block2 = _mm256_loadu_si256((__m256i*)p_in_128 + 2);
+        block3 = _mm256_loadu_si256((__m256i*)p_in_128 + 3);
+
+        // re-arrange as per spec
+        b1 = _mm256_shuffle_epi8(ctr1.ymm, swap_ctrx);
+        b2 = _mm256_shuffle_epi8(ctr2.ymm, swap_ctrx);
+        b3 = _mm256_shuffle_epi8(ctr3.ymm, swap_ctrx);
+        b4 = _mm256_shuffle_epi8(ctr4.ymm, swap_ctrx);
+
+        vaes::AESEncrypt(&(b1), &(b2), &(b3), &(b4), pkey128, nRounds);
+
+        block0 = _mm256_xor_si256(b1, block0);
+        block1 = _mm256_xor_si256(b2, block1);
+        block2 = _mm256_xor_si256(b3, block2);
+        block3 = _mm256_xor_si256(b4, block3);
+
+        ctr1.ymm = _mm256_add_epi32(ctr1.ymm, eighthilo);
+        ctr2.ymm = _mm256_add_epi32(ctr2.ymm, eighthilo);
+        ctr3.ymm = _mm256_add_epi32(ctr3.ymm, eighthilo);
+        ctr4.ymm = _mm256_add_epi32(ctr4.ymm, eighthilo);
+
+        _mm256_storeu_si256(reinterpret_cast<__m256i*>(p_out_128), block0);
+        _mm256_storeu_si256(reinterpret_cast<__m256i*>(p_out_128) + 1, block1);
+        _mm256_storeu_si256(reinterpret_cast<__m256i*>(p_out_128) + 2, block2);
+        _mm256_storeu_si256(reinterpret_cast<__m256i*>(p_out_128) + 3, block3);
+
+        p_in_128 += 8;
+        p_out_128 += 8;
+    }
 
     for (; blocks >= 4; blocks -= 4) {
         block0 = _mm256_loadu_si256((__m256i*)p_in_128);
