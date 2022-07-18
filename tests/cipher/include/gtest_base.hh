0b6c1aa951b15a796ac2069c0c61ac245170b519
bsinghpa <brijesh.singhpanwar@amd.com>
2022-07-18 15:14:19 +0530

    CIPHER:CROSSTEST:FUNCTION
    Migrated all cross tests to function

--- tests/cipher/include/gtest_base.hh
+++ tests/cipher/include/gtest_base.hh
@@ -40,12 +40,17 @@
 #ifdef USE_OSSL
 #include "openssl_base.hh"
 #endif
-enum ENC_DEC
+#include "rng_base.hh"
+typedef enum 
 {
     DECRYPT = 0,
     ENCRYPT
-};
+}enc_dec_t;
 
+typedef enum{
+    SMALL=0,
+    BIG
+}big_small_t;
 /**
  * returns respective string based on AES modes
  */
@@ -218,5 +223,166 @@ class TestingCore
     DataSet*       getDs() { return m_ds; }
     CipherTesting* getCipherHandler() { return m_cipherHandler; }
 };
+
+/**
+ * @brief Use the below parameters to control the range of small and big cross test
+ * 
+ */
+
+// Below in bytes
+#define SMALL_MAX_LOOP   160000
+#define SMALL_INC_LOOP   16
+#define SMALL_START_LOOP 16
+// Below in 0.1MB size
+#define BIG_MAX_LOOP   2
+#define BIG_INC_LOOP   1
+#define BIG_START_LOOP 1
+/**
+ * @brief 
+ * 
+ * @param e_d encryption or Decryption
+ * @param b_s big or small 
+ * @return record_t returns Enum for the type of encryption or decryption(big or small)
+ */
+record_t EncDecType(enc_dec_t e_d, big_small_t b_s){
+    if(b_s == BIG && e_d == ENCRYPT)
+        return BIG_ENC;
+    else if(b_s == BIG && e_d == DECRYPT)
+        return BIG_DEC;
+    else if(b_s == SMALL && e_d == ENCRYPT)
+        return SMALL_ENC;
+    else if(b_s == SMALL && e_d == DECRYPT)
+        return SMALL_DEC;
+    else std::cout<<"ERROR....invalid values of big_small or enc_dec"<<std::endl;
+        return SMALL_DEC;
+}
+
+/**
+ * @brief funtion to avoid repeated code in every cross test, can only be used for AES-CTR,AES-CBC,AES-OFB,AES-CFB
+ * 
+ * @param keySize keysize in bits(128,192 or 256)
+ * @param enc_dec (encryption or Decryption)
+ * @param mode AES modes (CTR, OFB, CBC and CFB)
+ * @param big_small Type (Big or Small) of test 
+ */
+void AesCrosstest(int keySize, enc_dec_t enc_dec, alc_aes_mode_t mode, big_small_t big_small)
+{
+    int         key_size = keySize;
+    int         LOOP_START, MAX_LOOP, INC_LOOP;
+    size_t      size = 1;
+    std::string enc_dec_str, big_small_str;
+    if (enc_dec == ENCRYPT)
+        enc_dec_str.assign("ENC");
+    else
+        enc_dec_str.assign("DEC");
+    if (big_small == BIG)
+        big_small_str.assign("BIG");
+    else
+        big_small_str.assign("SMALL");
+    /* Request from others to validate openssl with ipp */
+    TestingCore* alcpTC = nullptr;
+    if (oa_override) {
+        alcpTC = new TestingCore(OPENSSL, mode);
+        printErrors("ALCP is overriden!... OpenSSL is now main lib");
+        printErrors("ALCP is overriden!... Forcing IPP as extlib");
+        useipp  = true;
+        useossl = false;
+    } else {
+        alcpTC = new TestingCore(ALCP, mode);
+    }
+    TestingCore* extTC  = nullptr;
+    ExecRecPlay* fr     = nullptr;
+    RngBase      rb;
+    if (bbxreplay) {
+        fr = new ExecRecPlay("AES_"+ GetModeSTR(mode) + "_" + enc_dec_str + "_"
+                                 + std::to_string(key_size) + "_"
+                                 + big_small_str,
+                             "AES_"+ GetModeSTR(mode) + "_TEST_DATA",
+                             true);
+        fr->fastForward(EncDecType(enc_dec,big_small));
+    } else
+        fr = new ExecRecPlay("AES_"+ GetModeSTR(mode) + "_" + enc_dec_str + "_"
+                                 + std::to_string(key_size) + "_"
+                                 + big_small_str,
+                             "AES_"+ GetModeSTR(mode) + "_TEST_DATA",
+                             false);
+    /* Set extTC based on which external testing core user asks*/
+    try {
+        if (useossl)
+            extTC = new TestingCore(OPENSSL, mode);
+        else if (useipp)
+            extTC = new TestingCore(IPP, mode);
+        else {
+            printErrors("No Lib Specified!.. but trying OpenSSL");
+            extTC = new TestingCore(OPENSSL, mode);
+        }
+    } catch (const char* exc) {
+        std::cerr << exc << std::endl;
+    }
+    if (big_small == SMALL) {
+        LOOP_START = SMALL_START_LOOP;
+        MAX_LOOP   = SMALL_MAX_LOOP;
+        INC_LOOP   = SMALL_INC_LOOP;
+        size       = 1;
+    } else {
+        LOOP_START = BIG_START_LOOP;
+        MAX_LOOP   = BIG_MAX_LOOP;
+        INC_LOOP   = BIG_INC_LOOP;
+        size       = 16 * 10000000;
+    }
+
+    if (extTC != nullptr) {
+        for (int i = LOOP_START; i < MAX_LOOP; i += INC_LOOP) {
+            if (!bbxreplay)
+                fr->startRecEvent();
+            std::vector<uint8_t> pt, ct, key, iv;
+            if (!bbxreplay) {
+                if (enc_dec == ENCRYPT)
+                    pt = rb.genRandomBytes(i * size);
+                else if((enc_dec == DECRYPT))
+                    ct = rb.genRandomBytes(i * size);
+                key = rb.genRandomBytes(16);
+                iv  = rb.genRandomBytes(16);
+                if (enc_dec == ENCRYPT)
+                    fr->setRecEvent(key, iv, pt, EncDecType(enc_dec,big_small));
+                else if((enc_dec == DECRYPT))
+                    fr->setRecEvent(key, iv, ct, EncDecType(enc_dec,big_small));
+            } else {
+                fr->nextLog();
+                try {
+                    if (enc_dec == ENCRYPT)
+                        fr->getValues(&key, &iv, &pt);
+                    else if((enc_dec == DECRYPT))
+                        fr->getValues(&key, &iv, &ct);
+                    
+                } catch (std::string excp) {
+                    std::cout << excp << std::endl;
+                    exit(-1);
+                }
+            }
+            if (enc_dec == ENCRYPT) {
+                std::vector enc_1 =
+                    alcpTC->getCipherHandler()->testingEncrypt(pt, key, iv);
+                std::vector enc_2 =
+                    extTC->getCipherHandler()->testingEncrypt(pt, key, iv);
+                EXPECT_TRUE(ArraysMatch(enc_1, enc_2));
+            } else {
+                std::vector dec_1 =
+                    alcpTC->getCipherHandler()->testingDecrypt(ct, key, iv);
+                std::vector dec_2 =
+                    extTC->getCipherHandler()->testingDecrypt(ct, key, iv);
+                EXPECT_TRUE(ArraysMatch(dec_1, dec_2));
+            }
+            if (!bbxreplay) {
+                fr->dumpBlackBox();
+                fr->endRecEvent();
+                fr->dumpLog();
+            }
+        }
+        delete extTC;
+        delete alcpTC;
+    }
+    delete fr;
+}
 
 #endif
