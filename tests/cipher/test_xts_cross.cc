27af13ad02cc8fe15b91014821d1851944802ee9
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2022-08-17 07:44:42 +0000

    Crypto: XTS: Cross tests: IPP Updates for 128,256 big test type
    -- Fixed conflicts with amd-main (seperating enc, dec for xts)
    -- For xts, in general, size should be <= 2^20
    -- openssl cross tests are all passing.
    -- Made a workaround for ippcp, where not all block sizes are supported.

--- tests/cipher/test_xts_cross.cc
+++ tests/cipher/test_xts_cross.cc
@@ -55,6 +55,474 @@ ExecRecPlay* fr = nullptr;
 #define BIG_START_LOOP 1
 
 /* Testing Starts Here! */
+TEST(SYMMETRIC_DEC_128, 128_CROSS_CHECK_BIG)
+{
+    int key_size = 128;
+
+    // Request from others to validate openssl with ipp
+    // TODO: Upgrade flight recorder
+    TestingCore* alcpTC = nullptr;
+    if (oa_override) {
+        alcpTC = new TestingCore(OPENSSL, ALC_MODE);
+        printErrors("ALCP is overriden!... OpenSSL is now main lib");
+        printErrors("ALCP is overriden!... Forcing IPP as extlib");
+        useipp  = true;
+        useossl = false;
+    } else {
+        alcpTC = new TestingCore(ALCP, ALC_MODE);
+    }
+    TestingCore* extTC = nullptr;
+    RngBase      rb;
+    if (bbxreplay) {
+        fr = new ExecRecPlay(std::string(STR_MODE) + "_DEC_128_BIG",
+                             std::string(STR_MODE) + "_TEST_DATA",
+                             true);
+        fr->fastForward(SMALL_ENC);
+    } else
+        fr = new ExecRecPlay(std::string(STR_MODE) + "_DEC_128_BIG",
+                             std::string(STR_MODE) + "_TEST_DATA",
+                             false);
+
+    // Set extTC based on which external testing core user asks
+    try {
+        if (useossl)
+            extTC = new TestingCore(OPENSSL, ALC_MODE);
+        else if (useipp)
+            extTC = new TestingCore(IPP, ALC_MODE);
+        else {
+            printErrors("No Lib Specified!.. but trying OpenSSL");
+            extTC = new TestingCore(OPENSSL, ALC_MODE);
+        }
+    } catch (const char* exc) {
+        std::cerr << exc << std::endl;
+    }
+
+    int max_loop   = BIG_MAX_LOOP;
+    int start_loop = BIG_START_LOOP;
+    int inc_loop   = SMALL_INC_LOOP;
+
+    /* FIXME Need to update this for ippcp */
+    if (useipp) {
+        max_loop   = 32;
+        start_loop = 16;
+    }
+    if (extTC != nullptr) {
+        for (int i = start_loop; i < max_loop; i += inc_loop) {
+            /* max size supported by XTS is 2 ^ 20 = 1048576 */
+            size_t size = 524288 * i;
+
+            /* FIXME Need to update this for ippcp */
+            if (useipp)
+                size = i;
+
+            if (!bbxreplay)
+                fr->startRecEvent();
+            alcp_data_ex_t       data_alc, data_ext;
+            std::vector<uint8_t> ct, key, tkey, iv, add, out_pt_alc(size, 0),
+                out_pt_ext(size, 0);
+            if (!bbxreplay) {
+                ct   = rb.genRandomBytes(size);
+                key  = rb.genRandomBytes(key_size / 8);
+                tkey = rb.genRandomBytes(key_size / 8);
+                iv   = rb.genRandomBytes(12);
+
+                // ALC/Main Lib Data
+                data_alc.in    = &(ct[0]);
+                data_alc.inl   = ct.size();
+                data_alc.iv    = &(iv[0]);
+                data_alc.ivl   = iv.size();
+                data_alc.out   = &(out_pt_alc[0]);
+                data_alc.outl  = data_alc.inl;
+                data_alc.tkey  = &(tkey[0]);
+                data_alc.tkeyl = 16;
+
+                // External Lib Data
+                data_ext.in    = &(ct[0]);
+                data_ext.inl   = ct.size();
+                data_ext.iv    = &(iv[0]);
+                data_ext.ivl   = iv.size();
+                data_ext.out   = &(out_pt_ext[0]);
+                data_ext.outl  = data_alc.inl;
+                data_ext.tkey  = &(tkey[0]);
+                data_ext.tkeyl = 16;
+
+                fr->setRecEvent(key, iv, ct, BIG_DEC);
+            } else {
+                fr->nextLog();
+                try {
+                    fr->getValues(&key, &iv, &ct);
+                } catch (std::string excp) {
+                    std::cout << excp << std::endl;
+                    exit(-1);
+                }
+            }
+            alcpTC->getCipherHandler()->testingDecrypt(data_alc, key);
+            extTC->getCipherHandler()->testingDecrypt(data_ext, key);
+            ASSERT_TRUE(ArraysMatch(out_pt_alc, out_pt_ext));
+
+            if (!bbxreplay) {
+                fr->dumpBlackBox();
+                fr->endRecEvent();
+                fr->dumpLog();
+            }
+        }
+        delete extTC;
+        delete alcpTC;
+    }
+    delete fr;
+}
+
+TEST(SYMMETRIC_ENC_128, 128_CROSS_CHECK_BIG)
+{
+    int key_size = 128;
+
+    // Request from others to validate openssl with ipp
+    // TODO: Upgrade flight recorder
+    TestingCore* alcpTC = nullptr;
+    if (oa_override) {
+        alcpTC = new TestingCore(OPENSSL, ALC_MODE);
+        printErrors("ALCP is overriden!... OpenSSL is now main lib");
+        printErrors("ALCP is overriden!... Forcing IPP as extlib");
+        useipp  = true;
+        useossl = false;
+    } else {
+        alcpTC = new TestingCore(ALCP, ALC_MODE);
+    }
+    TestingCore* extTC = nullptr;
+    RngBase      rb;
+    if (bbxreplay) {
+        fr = new ExecRecPlay(std::string(STR_MODE) + "_DEC_128_BIG",
+                             std::string(STR_MODE) + "_TEST_DATA",
+                             true);
+        fr->fastForward(SMALL_ENC);
+    } else
+        fr = new ExecRecPlay(std::string(STR_MODE) + "_DEC_128_BIG",
+                             std::string(STR_MODE) + "_TEST_DATA",
+                             false);
+
+    // Set extTC based on which external testing core user asks
+    try {
+        if (useossl)
+            extTC = new TestingCore(OPENSSL, ALC_MODE);
+        else if (useipp)
+            extTC = new TestingCore(IPP, ALC_MODE);
+        else {
+            printErrors("No Lib Specified!.. but trying OpenSSL");
+            extTC = new TestingCore(OPENSSL, ALC_MODE);
+        }
+    } catch (const char* exc) {
+        std::cerr << exc << std::endl;
+    }
+
+    int max_loop   = BIG_MAX_LOOP;
+    int start_loop = BIG_START_LOOP;
+    int inc_loop   = SMALL_INC_LOOP;
+
+    /* FIXME Need to update this for ippcp */
+    if (useipp) {
+        max_loop   = 32;
+        start_loop = 16;
+    }
+    if (extTC != nullptr) {
+        for (int i = start_loop; i < max_loop; i += inc_loop) {
+            /* max size supported by XTS is 2 ^ 20 = 1048576 */
+            size_t size = 524288 * i;
+
+            /* FIXME Need to update this for ippcp */
+            if (useipp)
+                size = i;
+
+            if (!bbxreplay)
+                fr->startRecEvent();
+            alcp_data_ex_t       data_alc, data_ext;
+            std::vector<uint8_t> pt, key, tkey, iv, add, out_ct_alc(size, 0),
+                out_ct_ext(size, 0);
+            if (!bbxreplay) {
+                pt   = rb.genRandomBytes(size);
+                key  = rb.genRandomBytes(key_size / 8);
+                tkey = rb.genRandomBytes(key_size / 8);
+                iv   = rb.genRandomBytes(12);
+
+                // ALC/Main Lib Data
+                data_alc.in    = &(pt[0]);
+                data_alc.inl   = pt.size();
+                data_alc.iv    = &(iv[0]);
+                data_alc.ivl   = iv.size();
+                data_alc.out   = &(out_ct_alc[0]);
+                data_alc.outl  = data_alc.inl;
+                data_alc.tkey  = &(tkey[0]);
+                data_alc.tkeyl = 16;
+
+                // External Lib Data
+                data_ext.in    = &(pt[0]);
+                data_ext.inl   = pt.size();
+                data_ext.iv    = &(iv[0]);
+                data_ext.ivl   = iv.size();
+                data_ext.out   = &(out_ct_ext[0]);
+                data_ext.outl  = data_alc.inl;
+                data_ext.tkey  = &(tkey[0]);
+                data_ext.tkeyl = 16;
+
+                fr->setRecEvent(key, iv, pt, BIG_ENC);
+            } else {
+                fr->nextLog();
+                try {
+                    fr->getValues(&key, &iv, &pt);
+                } catch (std::string excp) {
+                    std::cout << excp << std::endl;
+                    exit(-1);
+                }
+            }
+            alcpTC->getCipherHandler()->testingEncrypt(data_alc, key);
+            extTC->getCipherHandler()->testingEncrypt(data_ext, key);
+            ASSERT_TRUE(ArraysMatch(out_ct_alc, out_ct_ext));
+
+            if (!bbxreplay) {
+                fr->dumpBlackBox();
+                fr->endRecEvent();
+                fr->dumpLog();
+            }
+        }
+        delete extTC;
+        delete alcpTC;
+    }
+    delete fr;
+}
+
+TEST(SYMMETRIC_ENC_256, 256_CROSS_CHECK_BIG)
+{
+    int key_size = 256;
+
+    // Request from others to validate openssl with ipp
+    // TODO: Upgrade flight recorder
+    TestingCore* alcpTC = nullptr;
+    if (oa_override) {
+        alcpTC = new TestingCore(OPENSSL, ALC_MODE);
+        printErrors("ALCP is overriden!... OpenSSL is now main lib");
+        printErrors("ALCP is overriden!... Forcing IPP as extlib");
+        useipp  = true;
+        useossl = false;
+    } else {
+        alcpTC = new TestingCore(ALCP, ALC_MODE);
+    }
+    TestingCore* extTC = nullptr;
+    RngBase      rb;
+    if (bbxreplay) {
+        fr = new ExecRecPlay(std::string(STR_MODE) + "_ENC_256_BIG",
+                             std::string(STR_MODE) + "_TEST_DATA",
+                             true);
+        fr->fastForward(BIG_ENC);
+    } else
+        fr = new ExecRecPlay(std::string(STR_MODE) + "_ENC_256_BIG",
+                             std::string(STR_MODE) + "_TEST_DATA",
+                             false);
+
+    // Set extTC based on which external testing core user asks
+    try {
+        if (useossl)
+            extTC = new TestingCore(OPENSSL, ALC_MODE);
+        else if (useipp)
+            extTC = new TestingCore(IPP, ALC_MODE);
+        else {
+            printErrors("No Lib Specified!.. but trying OpenSSL");
+            extTC = new TestingCore(OPENSSL, ALC_MODE);
+        }
+    } catch (const char* exc) {
+        std::cerr << exc << std::endl;
+    }
+
+    int max_loop   = BIG_MAX_LOOP;
+    int start_loop = BIG_START_LOOP;
+    int inc_loop   = SMALL_INC_LOOP;
+
+    /* FIXME Need to update this for ippcp */
+    if (useipp) {
+        max_loop   = 32;
+        start_loop = 16;
+    }
+    if (extTC != nullptr) {
+        for (int i = start_loop; i < max_loop; i += inc_loop) {
+            /* max size supported by XTS is 2 ^ 20 = 1048576 */
+            size_t size = 524288 * i;
+
+            /* FIXME Need to update this for ippcp */
+            if (useipp)
+                size = i;
+
+            if (!bbxreplay)
+                fr->startRecEvent();
+            alcp_data_ex_t       data_alc, data_ext;
+            std::vector<uint8_t> pt, key, tkey, iv, add, out_ct_alc(size, 0),
+                out_ct_ext(size, 0);
+            if (!bbxreplay) {
+                pt   = rb.genRandomBytes(size);
+                key  = rb.genRandomBytes(key_size / 8);
+                tkey = rb.genRandomBytes(key_size / 8);
+                iv   = rb.genRandomBytes(12);
+
+                // ALC/Main Lib Data
+                data_alc.in    = &(pt[0]);
+                data_alc.inl   = pt.size();
+                data_alc.iv    = &(iv[0]);
+                data_alc.ivl   = iv.size();
+                data_alc.out   = &(out_ct_alc[0]);
+                data_alc.outl  = data_alc.inl;
+                data_alc.tkey  = &(tkey[0]);
+                data_alc.tkeyl = 16;
+
+                // External Lib Data
+                data_ext.in    = &(pt[0]);
+                data_ext.inl   = pt.size();
+                data_ext.iv    = &(iv[0]);
+                data_ext.ivl   = iv.size();
+                data_ext.out   = &(out_ct_ext[0]);
+                data_ext.outl  = data_alc.inl;
+                data_ext.tkey  = &(tkey[0]);
+                data_ext.tkeyl = 16;
+
+                fr->setRecEvent(key, iv, pt, BIG_ENC);
+            } else {
+                fr->nextLog();
+                try {
+                    fr->getValues(&key, &iv, &pt);
+                } catch (std::string excp) {
+                    std::cout << excp << std::endl;
+                    exit(-1);
+                }
+            }
+            alcpTC->getCipherHandler()->testingEncrypt(data_alc, key);
+            extTC->getCipherHandler()->testingEncrypt(data_ext, key);
+            ASSERT_TRUE(ArraysMatch(out_ct_alc, out_ct_ext));
+
+            if (!bbxreplay) {
+                fr->dumpBlackBox();
+                fr->endRecEvent();
+                fr->dumpLog();
+            }
+        }
+        delete extTC;
+        delete alcpTC;
+    }
+    delete fr;
+}
+
+TEST(SYMMETRIC_DEC_256, 256_CROSS_CHECK_BIG)
+{
+    int key_size = 256;
+
+    // Request from others to validate openssl with ipp
+    // TODO: Upgrade flight recorder
+    TestingCore* alcpTC = nullptr;
+    if (oa_override) {
+        alcpTC = new TestingCore(OPENSSL, ALC_MODE);
+        printErrors("ALCP is overriden!... OpenSSL is now main lib");
+        printErrors("ALCP is overriden!... Forcing IPP as extlib");
+        useipp  = true;
+        useossl = false;
+    } else {
+        alcpTC = new TestingCore(ALCP, ALC_MODE);
+    }
+    TestingCore* extTC = nullptr;
+    RngBase      rb;
+    if (bbxreplay) {
+        fr = new ExecRecPlay(std::string(STR_MODE) + "_DEC_256_BIG",
+                             std::string(STR_MODE) + "_TEST_DATA",
+                             true);
+        fr->fastForward(BIG_DEC);
+    } else
+        fr = new ExecRecPlay(std::string(STR_MODE) + "_DEC_256_BIG",
+                             std::string(STR_MODE) + "_TEST_DATA",
+                             false);
+
+    // Set extTC based on which external testing core user asks
+    try {
+        if (useossl)
+            extTC = new TestingCore(OPENSSL, ALC_MODE);
+        else if (useipp)
+            extTC = new TestingCore(IPP, ALC_MODE);
+        else {
+            printErrors("No Lib Specified!.. but trying OpenSSL");
+            extTC = new TestingCore(OPENSSL, ALC_MODE);
+        }
+    } catch (const char* exc) {
+        std::cerr << exc << std::endl;
+    }
+
+    int max_loop   = BIG_MAX_LOOP;
+    int start_loop = BIG_START_LOOP;
+    int inc_loop   = SMALL_INC_LOOP;
+
+    /* FIXME Need to update this for ippcp */
+    if (useipp) {
+        max_loop   = 32;
+        start_loop = 16;
+    }
+    if (extTC != nullptr) {
+        for (int i = start_loop; i < max_loop; i += inc_loop) {
+            /* max size supported by XTS is 2 ^ 20 = 1048576 */
+            size_t size = 524288 * i;
+
+            /* FIXME Need to update this for ippcp */
+            if (useipp)
+                size = i;
+
+            if (!bbxreplay)
+                fr->startRecEvent();
+            alcp_data_ex_t       data_alc, data_ext;
+            std::vector<uint8_t> ct, key, tkey, iv, add, out_pt_alc(size, 0),
+                out_pt_ext(size, 0);
+            if (!bbxreplay) {
+                ct   = rb.genRandomBytes(size);
+                key  = rb.genRandomBytes(key_size / 8);
+                tkey = rb.genRandomBytes(key_size / 8);
+                iv   = rb.genRandomBytes(12);
+
+                // ALC/Main Lib Data
+                data_alc.in    = &(ct[0]);
+                data_alc.inl   = ct.size();
+                data_alc.iv    = &(iv[0]);
+                data_alc.ivl   = iv.size();
+                data_alc.out   = &(out_pt_alc[0]);
+                data_alc.outl  = data_alc.inl;
+                data_alc.tkey  = &(tkey[0]);
+                data_alc.tkeyl = 16;
+
+                // External Lib Data
+                data_ext.in    = &(ct[0]);
+                data_ext.inl   = ct.size();
+                data_ext.iv    = &(iv[0]);
+                data_ext.ivl   = iv.size();
+                data_ext.out   = &(out_pt_ext[0]);
+                data_ext.outl  = data_alc.inl;
+                data_ext.tkey  = &(tkey[0]);
+                data_ext.tkeyl = 16;
+
+                fr->setRecEvent(key, iv, ct, BIG_DEC);
+            } else {
+                fr->nextLog();
+                try {
+                    fr->getValues(&key, &iv, &ct);
+                } catch (std::string excp) {
+                    std::cout << excp << std::endl;
+                    exit(-1);
+                }
+            }
+            alcpTC->getCipherHandler()->testingDecrypt(data_alc, key);
+            extTC->getCipherHandler()->testingDecrypt(data_ext, key);
+            ASSERT_TRUE(ArraysMatch(out_pt_alc, out_pt_ext));
+
+            if (!bbxreplay) {
+                fr->dumpBlackBox();
+                fr->endRecEvent();
+                fr->dumpLog();
+            }
+        }
+        delete extTC;
+        delete alcpTC;
+    }
+    delete fr;
+}
+
 TEST(SYMMETRIC_ENC_128, 128_CROSS_CHECK_SMALL)
 {
     int key_size = 128;
@@ -212,7 +680,6 @@ TEST(SYMMETRIC_DEC_128, 128_CROSS_CHECK_SMALL)
     }
     if (extTC != nullptr) {
         for (int i = SMALL_START_LOOP; i < max_loop; i += SMALL_INC_LOOP) {
-            fprintf(stderr, "RUNNING: %d\n", i);
             if (!bbxreplay)
                 fr->startRecEvent();
             alcp_data_ex_t       data_alc, data_ext;
@@ -270,10 +737,10 @@ TEST(SYMMETRIC_DEC_128, 128_CROSS_CHECK_SMALL)
     delete fr;
 }
 
-/* 256 */
-TEST(SYMMETRIC_ENC_256, 256_CROSS_CHECK_BIG)
+TEST(SYMMETRIC_ENC_256, 256_CROSS_CHECK_SMALL)
 {
     int key_size = 256;
+
     // Request from others to validate openssl with ipp
     // TODO: Upgrade flight recorder
     TestingCore* alcpTC = nullptr;
@@ -311,6 +778,7 @@ TEST(SYMMETRIC_ENC_256, 256_CROSS_CHECK_BIG)
     } catch (const char* exc) {
         std::cerr << exc << std::endl;
     }
+
     int max_loop = SMALL_MAX_LOOP;
 
     /*FIXME Need to update this for ippcp */
@@ -376,9 +844,10 @@ TEST(SYMMETRIC_ENC_256, 256_CROSS_CHECK_BIG)
     delete fr;
 }
 
-TEST(SYMMETRIC_DEC_256, 256_CROSS_CHECK_BIG)
+TEST(SYMMETRIC_DEC_256, 256_CROSS_CHECK_SMALL)
 {
     int key_size = 256;
+
     // Request from others to validate openssl with ipp
     // TODO: Upgrade flight recorder
     TestingCore* alcpTC = nullptr;
@@ -416,6 +885,7 @@ TEST(SYMMETRIC_DEC_256, 256_CROSS_CHECK_BIG)
     } catch (const char* exc) {
         std::cerr << exc << std::endl;
     }
+
     int max_loop = SMALL_MAX_LOOP;
 
     /*FIXME Need to update this for ippcp */
@@ -480,6 +950,7 @@ TEST(SYMMETRIC_DEC_256, 256_CROSS_CHECK_BIG)
     }
     delete fr;
 }
+/* end of list of tests */
 
 int
 main(int argc, char** argv)
