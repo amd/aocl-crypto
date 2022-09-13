e763b3b502999bc042487617866a6a0508d68454
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2022-09-13 13:08:48 +0000

    Crypto: Digest: Refactor Cross tests
    -- Add function to run cross tests based on param
    -- Move different SHA2 digest cross, KAT tests and bench to common function.
    -- Corresponding changes to gtest_base header as well

--- tests/include/digest/gtest_base.hh
+++ tests/include/digest/gtest_base.hh
@@ -32,5 +32,145 @@
 
 #include "gtest_common.hh"
 #include <vector>
+#include "digest/alc_base.hh"
+#include "digest/base.hh"
+#include "digest/gtest_base.hh"
+#include "rng_base.hh"
+#include <alcp/alcp.h>
+#include <iostream>
+#include <string.h>
+
+#ifdef USE_IPP
+#include "digest/ipp_base.hh"
+#endif
+#ifdef USE_OSSL
+#include "digest/openssl_base.hh"
+#endif
+
+#define MAX_LOOP   160000
+#define INC_LOOP   16
+#define START_LOOP 16
+
+enum _alc_sha2_mode
+GetSHA2Mode(int HashSize)
+{
+    switch (HashSize) {
+        case 224:
+            return ALC_SHA2_224;
+        case 256:
+            return ALC_SHA2_256;
+        case 384:
+            return ALC_SHA2_384;
+        case 512:
+            return ALC_SHA2_512;
+        default:
+            return ALC_SHA2_224;
+    }
+}
+
+enum _alc_digest_len
+GetSHA2Len(int HashSize)
+{
+    switch (HashSize) {
+        case 224:
+            return ALC_DIGEST_LEN_224;
+        case 256:
+            return ALC_DIGEST_LEN_256;
+        case 384:
+            return ALC_DIGEST_LEN_384;
+        case 512:
+            return ALC_DIGEST_LEN_512;
+        default:
+            return ALC_DIGEST_LEN_128;
+    }
+}
+
+record_t
+GetSHA2Record(int HashSize)
+{
+    switch (HashSize)
+    {
+    case 224:
+        return SHA2_224;
+    case 256:
+        return SHA2_256;
+    case 384:
+        return SHA2_384;
+    case 512:
+        return SHA2_512;
+    default:
+        return SHA2_224;
+    }
+}
+
+ExecRecPlay* fr;
+void SHA2_CrossTest(int HashSize)
+{
+    alc_error_t            error;
+    std::vector<uint8_t>   data;
+    std::vector<uint8_t>   digestAlcp(HashSize/8, 0);
+    std::vector<uint8_t>   digestExt(HashSize/8, 0);
+    const alc_sha2_mode_t  alc_mode       = GetSHA2Mode(HashSize);
+    const alc_digest_len_t alc_digest_len = GetSHA2Len(HashSize);
+    AlcpDigestBase         adb(alc_mode, ALC_DIGEST_TYPE_SHA2, alc_digest_len);
+    RngBase                rng;
+    DigestBase*            db;
+    DigestBase*            extDb = nullptr;
+    db                           = &adb;
+    if (bbxreplay) {
+        fr = new ExecRecPlay("SHA2_" + std::to_string(HashSize), true);
+        fr->fastForward(GetSHA2Record(HashSize));
+    } else
+        fr = new ExecRecPlay("SHA2_" + std::to_string(HashSize), false);
+#ifdef USE_IPP
+    IPPDigestBase idb(alc_mode, ALC_DIGEST_TYPE_SHA2, alc_digest_len);
+    if (useipp == true)
+        extDb = &idb;
+#endif
+#ifdef USE_OSSL
+    OpenSSLDigestBase odb(alc_mode, ALC_DIGEST_TYPE_SHA2, alc_digest_len);
+    if ((useossl == true) || (extDb == nullptr)) // Select OpenSSL by default
+        extDb = &odb;
+#endif
+    if (extDb == nullptr) {
+        printErrors("No external lib selected!");
+        exit(-1);
+    }
+
+    // TODO: Improve the incementor and start condition of forloop
+    for (int i = START_LOOP; i < MAX_LOOP; i += INC_LOOP) {
+        if (!bbxreplay) {
+            fr->startRecEvent();
+            try {
+                data = rng.genRandomBytes(i);
+                fr->setRecEvent(data, GetSHA2Record(HashSize));
+            } catch (const char* error) {
+                printErrors(error);
+                exit(-1);
+            }
+        } else {
+            fr->nextLog();
+            fr->getValues(&data);
+        }
+
+        error = db->digest_function(
+            &(data[0]), data.size(), &(digestAlcp[0]), digestAlcp.size());
+        error = extDb->digest_function(
+            &(data[0]), data.size(), &(digestExt[0]), digestExt.size());
+        db->init(alc_mode, ALC_DIGEST_TYPE_SHA2, alc_digest_len);
+        extDb->init(alc_mode, ALC_DIGEST_TYPE_SHA2, alc_digest_len);
+        if (alcp_is_error(error)) {
+            printf("Error");
+            return;
+        }
+        EXPECT_TRUE(ArraysMatch(digestAlcp, digestExt, i));
+        if (!bbxreplay) {
+            fr->dumpBlackBox();
+            fr->endRecEvent();
+            fr->dumpLog();
+        }
+    }
+    delete fr;
+}
 
 #endif
