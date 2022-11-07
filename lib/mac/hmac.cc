97500e132edda888c0f6dbefafb746f37d7f8aa7
Abhijith N Raj <abhijith.raj@amd.com>
2022-11-07 14:44:39 +0530

    Crypto:MAC:HMAC: Added implementation code for HMAC

--- lib/mac/hmac.cc
+++ lib/mac/hmac.cc
@@ -27,22 +27,259 @@
  */
 
 #include "mac/hmac.hh"
+#include "utils/copy.hh"
+#include <immintrin.h>
+
+std::vector<Uint8>
+exor_vect(std::vector<Uint8> a, std::vector<Uint8> b)
+{
+    assert(a.size() == b.size());
+    std::vector<Uint8> d(a.size(), 0);
+    for (size_t i = 0; i < a.size(); i++) {
+        d.at(i) = a.at(i) ^ b.at(i);
+    }
+    return d;
+}
+
 namespace alcp::mac {
-alc_error_t
-Hmac::update(const Uint8* pMsgBuf, Uint64 size)
+class Hmac::Impl
 {
-    return ALC_ERROR_NONE;
+
+  private:
+    std::vector<Uint8>    key;
+    Uint32                keylen;
+    std::vector<Uint8>    k0, opad, ipad;
+    Uint32                output_hash_size{};
+    std::vector<Uint8>    output_mac;
+    Uint32                input_block_length{};
+    alcp::digest::Digest* p_digest;
+    hmac_state_t          state = INVALID;
+
+    std::vector<Uint8> k0_xor_opad, k0_xor_ipad;
+
+  public:
+    Impl(const alc_mac_info_t mac_info, alcp::digest::Digest* p_digest)
+        : p_digest{ p_digest }
+    {
+        alc_error_t    err;
+        alc_key_info_t kinfo = mac_info.mi_keyinfo;
+        input_block_length   = p_digest->getInputBlockSize();
+
+        if (input_block_length == 0) {
+            // throw std::length_error(
+            //     "ERROR: Block Length of the hash function cannot be 0");
+            state = INVALID;
+        }
+        output_hash_size = p_digest->getHashSize();
+
+        if (output_hash_size == 0) {
+            // throw std::length_error("ERROR: Digest Hash Size cannot be 0");
+            state = INVALID;
+        }
+
+        output_mac.assign(output_hash_size, 0);
+        err = validate_keys(kinfo);
+        if (err) {
+            state = INVALID;
+            return;
+        }
+        opad        = std::vector<Uint8>(input_block_length, 0x5c);
+        ipad        = std::vector<Uint8>(input_block_length, 0x36);
+        k0          = get_k0(input_block_length);
+        k0_xor_ipad = exor_vect(k0, ipad);
+        k0_xor_opad = exor_vect(k0, opad);
+        // start the hash calculation
+        err = calculate_hash(p_digest, k0_xor_ipad);
+        if (err) {
+        } else {
+            state = VALID;
+        }
+    }
+
+  public:
+    Uint64 getHashSize()
+    {
+        if (state == VALID)
+            return output_hash_size;
+        else
+            return 0;
+    }
+    hmac_state_t getState() const { return state; };
+    alc_error_t  update(std::vector<Uint8> buff)
+    {
+        alc_error_t err = ALC_ERROR_NONE;
+        if (getState() == VALID)
+            err = calculate_hash(p_digest, buff);
+        else {
+            err = ALC_ERROR_BAD_STATE;
+        }
+        return err;
+    }
+
+    alc_error_t update(const Uint8* buff, Uint64 size)
+    {
+        alc_error_t err = ALC_ERROR_NONE;
+        if (getState() == VALID)
+            err = calculate_hash(p_digest, buff, size, nullptr);
+        else
+            err = ALC_ERROR_BAD_STATE;
+
+        return err;
+    }
+    alc_error_t finalize(const Uint8* buff, Uint64 size)
+    {
+        if (getState() == VALID) {
+            if (sizeof(buff) != 0 && size != 0)
+                calculate_hash(p_digest, buff, size, nullptr);
+            p_digest->finalize(nullptr, 0);
+
+            std::vector<Uint8> h1(output_hash_size, 0);
+            p_digest->copyHash(&(h1.at(0)), h1.size());
+            p_digest->reset();
+            std::vector<Uint8> k0_xor_opad_ct_h1;
+            k0_xor_opad_ct_h1.insert(k0_xor_opad_ct_h1.end(),
+                                     k0_xor_opad.begin(),
+                                     k0_xor_opad.end());
+            k0_xor_opad_ct_h1.insert(
+                k0_xor_opad_ct_h1.end(), h1.begin(), h1.end());
+            calculate_hash(p_digest, k0_xor_opad_ct_h1);
+
+            p_digest->finalize(nullptr, 0);
+            p_digest->copyHash(&(output_mac.at(0)), output_mac.size());
+            p_digest->reset();
+        }
+        return ALC_ERROR_NONE;
+    }
+
+    alc_error_t copyHash(Uint8* buff, Uint64 size) const
+    {
+        alc_error_t err = ALC_ERROR_NONE;
+        if (getState() == VALID)
+            alcp::utils::CopyBytes(buff, &output_mac.at(0), size);
+        else
+            err = ALC_ERROR_BAD_STATE;
+        return err;
+    }
+
+  private:
+    alc_error_t validate_keys(const alc_key_info_t& rKeyInfo)
+    {
+        // For RAW assignments
+        switch (rKeyInfo.fmt) {
+
+            case ALC_KEY_FMT_RAW:
+                keylen = rKeyInfo.len;
+                if (keylen == 0) {
+                    // std::cout << "ERROR:Key Length Cannot be Zero" <<
+                    // std::endl;
+                    return ALC_ERROR_INVALID_SIZE;
+                }
+                if (rKeyInfo.key) {
+                    key =
+                        std::vector<Uint8>(rKeyInfo.key, rKeyInfo.key + keylen);
+                } else {
+                    // std::cout << "ERROR:Key Cannot be NULL" << std::endl;
+                    return ALC_ERROR_NOT_PERMITTED;
+                }
+                break;
+            case ALC_KEY_FMT_BASE64:
+                // TODO: For base64 conversions
+                return ALC_ERROR_NOT_SUPPORTED; // remove this return when
+                                                // above todo is resolved.
+                break;
+            // TODO: Subsequest switch cases for other formats
+            default:
+                return ALC_ERROR_NOT_SUPPORTED;
+        }
+        return ALC_ERROR_NONE;
+    }
+
+    std::vector<Uint8> get_k0(Uint32 block_len)
+    {
+        std::vector<Uint8> k0;
+        if (block_len == keylen) {
+            k0 = key;
+        } else if (keylen < block_len) {
+            int L                       = block_len - keylen;
+            k0                          = key;
+            std::vector<Uint8> zerovect = std::vector<Uint8>(L, 0);
+            k0.insert(k0.end(), zerovect.begin(), zerovect.end());
+        } else if (keylen > block_len) {
+            // Optimization: Reusing p_digest for calculating this sha
+            p_digest->reset();
+            std::vector<Uint8> hash = std::vector<Uint8>(output_hash_size, 0);
+            p_digest->update(&(key.at(0)), key.size());
+            p_digest->finalize(nullptr, 0);
+            p_digest->copyHash(&(hash.at(0)), output_hash_size);
+            p_digest->reset();
+
+            int L = block_len - output_hash_size;
+
+            std::vector<Uint8> zerovect = std::vector<Uint8>(L, 0);
+            hash.insert(hash.end(), zerovect.begin(), zerovect.end());
+            k0 = hash;
+        }
+        return k0;
+    }
+
+    alc_error_t calculate_hash(alcp::digest::Digest* p_digest,
+                               std::vector<Uint8>    input)
+    {
+        return calculate_hash(p_digest, &(input.at(0)), input.size(), nullptr);
+    }
+
+    alc_error_t calculate_hash(alcp::digest::Digest* p_digest,
+                               const Uint8*          input,
+                               Uint64                len,
+                               Uint8*                output)
+    {
+        alc_error_t err;
+        err = p_digest->update(input, len);
+        p_digest->copyHash(output, output_hash_size);
+        return err;
+    }
+};
+
+Hmac::Hmac(const alc_mac_info_t mac_info, alcp::digest::Digest* p_digest)
+    : p_digest{ p_digest }
+    , m_pimpl{ std::make_unique<Hmac::Impl>(mac_info, p_digest) }
+{}
+Hmac::~Hmac() {}
+
+alc_error_t
+Hmac::update(std::vector<Uint8> buff)
+{
+    return m_pimpl->update(buff);
 }
 
 alc_error_t
-Hmac::finalize(const Uint8* pMsgBuf, Uint64 size)
+Hmac::update(const Uint8* buff, Uint64 size)
 {
-    return ALC_ERROR_NONE;
-}
-alc_error_t
-Hmac::copyHash(Uint8* pHashBuf, Uint64 size) const
-{
-    return ALC_ERROR_NONE;
+    return m_pimpl->update(buff, size);
 }
 
+alc_error_t
+Hmac::finalize(const Uint8* buff, Uint64 size)
+{
+
+    return m_pimpl->finalize(buff, size);
+}
+
+alc_error_t
+Hmac::copyHash(Uint8* buff, Uint64 size) const
+{
+    return m_pimpl->copyHash(buff, size);
+}
+
+Uint64
+Hmac::getHashSize()
+{
+    return m_pimpl->getHashSize();
+}
+
+hmac_state_t
+Hmac::getState() const
+{
+    return m_pimpl->getState();
+}
 } // namespace alcp::mac
