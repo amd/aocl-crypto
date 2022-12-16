3788a82cefbf9dcd1ed934a1b15aaaa271fd4a72
Abhijith N Raj <abhijith.raj@amd.com>
2022-12-16 07:33:20 +0000

    MAC:HMAC Added more testcases to HMAC Unit Test

--- lib/mac/tests/hmac_unit_test.cc
+++ lib/mac/tests/hmac_unit_test.cc
@@ -29,6 +29,7 @@
 #include "alcp/alcp.h"
 #include "alcp/types.h"
 #include "digest/sha2_384.hh"
+#include "digest/sha3.hh"
 #include "mac/hmac.hh"
 #include "gtest/gtest.h"
 
@@ -104,9 +105,9 @@ typedef std::map<const std::string, param_tuple> known_answer_map_t;
 
 //Order is key,ciphertext,mac
 //B: Input Block Size
-known_answer_map_t KATSHA256Dataset {
+known_answer_map_t KAT_ShaDataset {
     {
-        "SHA256_KEYLEN_EQ_B",
+        "SHA2_256_KEYLEN_EQ_B",
         {
             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f",
             "53616d706c65206d65737361676520666f72206b65796c656e3d626c6f636b6c656e",
@@ -115,7 +116,7 @@ known_answer_map_t KATSHA256Dataset {
 
     },
     {
-        "SHA256_KEYLEN_LT_B",
+        "SHA2_256_KEYLEN_LT_B",
         {
             "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F",
             "53616D706C65206D65737361676520666F72206B65796C656E3C626C6F636B6C656E",
@@ -124,7 +125,7 @@ known_answer_map_t KATSHA256Dataset {
         }
     },
     {
-        "SHA256_KEYLEN_GT_B",
+        "SHA2_256_KEYLEN_LT_B",
         {
             "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F30"
             "3132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F60616263",
@@ -133,7 +134,7 @@ known_answer_map_t KATSHA256Dataset {
         }
     },
     {
-        "SHA224_KEYLEN_LT_B",
+        "SHA2_224_KEYLEN_LT_B",
         {
             "cf127579d6b2b0b3a607a6314bf8733061c32a043593195527544f8753c65c7a70d05874f718275b88d0fa288bd3199813f0",
             "fa7e18cc5443981f22c0a5aba2117915f89c7781c34f61f9f429cb13e0fcd0ce947103be684ca869d7f125f08d27b3f2c21d59adc7ab1b66ded96f0b4fa5f018b80156b7a51ca62b60e2a66e0bc69419ebbf178507907630f24d0862e51bec101037f900323af82e689b116f427584541c8a9a51ac89da1ed78c7f5ec9e52a7f",
@@ -141,23 +142,168 @@ known_answer_map_t KATSHA256Dataset {
 
         }
     },
+    {
+        "SHA2_224_KEYLEN_EQ_B",
         {
-        "SHA384_KEYLEN_LT_B",
+            "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F",
+            "53616D706C65206D65737361676520666F72206B65796C656E3D626C6F636B6C656E",
+            "C7405E3AE058E8CD30B08B4140248581ED174CB34E1224BCC1EFC81B"
+        }
+    },
+    {
+        "SHA2_224_KEYLEN_GT_B",
+        {
+            "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F60616263",
+            "53616D706C65206D65737361676520666F72206B65796C656E3D626C6F636B6C656E",
+            "91C52509E5AF8531601AE6230099D90BEF88AAEFB961F4080ABC014D"
+        }
+    },
+    {
+        "SHA2_384_KEYLEN_LT_B",
         {
             "5eab0dfa27311260d7bddcf77112b23d8b42eb7a5d72a5a318e1ba7e7927f0079dbb701317b87a3340e156dbcee28ec3a8d9",
             "f41380123ccbec4c527b425652641191e90a17d45e2f6206cf01b5edbe932d41cc8a2405c3195617da2f420535eed422ac6040d9cd65314224f023f3ba730d19db9844c71c329c8d9d73d04d8c5f244aea80488292dc803e772402e72d2e9f1baba5a6004f0006d822b0b2d65e9e4a302dd4f776b47a972250051a701fab2b70",
             "7cf5a06156ad3de5405a5d261de90275f9bb36de45667f84d08fbcb308ca8f53a419b07deab3b5f8ea231c5b036f8875"
 
         }
-        
     },
-       {"SHA512_KEYLEN_LT_B",
+    {
+        "SHA2_384_KEYLEN_EQ_B",
+        {
+            "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F606162636465666768696A6B6C6D6E6F707172737475767778797A7B7C7D7E7F",
+            "53616D706C65206D65737361676520666F72206B65796C656E3D626C6F636B6C656E",
+            "63C5DAA5E651847CA897C95814AB830BEDEDC7D25E83EEF9195CD45857A37F448947858F5AF50CC2B1B730DDF29671A9"
+        }
+    },
+    {
+        "SHA2_384_KEYLEN_GT_B",
+        {
+            "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F606162636465666768696A6B6C6D6E6F707172737475767778797A7B7C7D7E7F808182838485868788898A8B8C8D8E8F909192939495969798999A9B9C9D9E9FA0A1A2A3A4A5A6A7A8A9AAABACADAEAFB0B1B2B3B4B5B6B7B8B9BABBBCBDBEBFC0C1C2C3C4C5C6C7",
+            "53616D706C65206D65737361676520666F72206B65796C656E3D626C6F636B6C656E",
+            "5B664436DF69B0CA22551231A3F0A3D5B4F97991713CFA84BFF4D0792EFF96C27DCCBBB6F79B65D548B40E8564CEF594"
+        }
+    },
+    {
+        "SHA2_512_KEYLEN_LT_B",
          {
             "57c2eb677b5093b9e829ea4babb50bde55d0ad59fec34a618973802b2ad9b78e26b2045dda784df3ff90ae0f2cc51ce39cf54867320ac6f3ba2c6f0d72360480c96614ae66581f266c35fb79fd28774afd113fa5187eff9206d7cbe90dd8bf67c844e202",
             "2423dff48b312be864cb3490641f793d2b9fb68a7763b8e298c86f42245e4540eb01ae4d2d4500370b1886f23ca2cf9701704cad5bd21ba87b811daf7a854ea24a56565ced425b35e40e1acbebe03603e35dcf4a100e57218408a1d8dbcc3b99296cfea931efe3ebd8f719a6d9a15487b9ad67eafedf15559ca42445b0f9b42e",
             "33c511e9bc2307c62758df61125a980ee64cefebd90931cb91c13742d4714c06de4003faf3c41c06aefc638ad47b21906e6b104816b72de6269e045a1f4429d4"
         }
-       }
+    },
+    {
+        "SHA2_512_KEYLEN_EQ_B",
+         {
+            "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F606162636465666768696A6B6C6D6E6F707172737475767778797A7B7C7D7E7F",
+            "53616D706C65206D65737361676520666F72206B65796C656E3D626C6F636B6C656E",
+            "FC25E240658CA785B7A811A8D3F7B4CA48CFA26A8A366BF2CD1F836B05FCB024BD36853081811D6CEA4216EBAD79DA1CFCB95EA4586B8A0CE356596A55FB1347"
+        }
+    },
+    {
+        "SHA2_512_KEYLEN_GT_B",
+         {
+            "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F606162636465666768696A6B6C6D6E6F707172737475767778797A7B7C7D7E7F808182838485868788898A8B8C8D8E8F909192939495969798999A9B9C9D9E9FA0A1A2A3A4A5A6A7A8A9AAABACADAEAFB0B1B2B3B4B5B6B7B8B9BABBBCBDBEBFC0C1C2C3C4C5C6C7",
+            "53616D706C65206D65737361676520666F72206B65796C656E3D626C6F636B6C656E",
+            "D93EC8D2DE1AD2A9957CB9B83F14E76AD6B5E0CCE285079A127D3B14BCCB7AA7286D4AC0D4CE64215F2BC9E6870B33D97438BE4AAA20CDA5C5A912B48B8E27F3"
+        }
+    },
+    {
+        "SHA3_224_KEYLEN_LT_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b",
+            "53616d706c65206d65737361676520666f72206b65796c656e3c626c6f636b6c656e",
+            "332cfd59347fdb8e576e77260be4aba2d6dc53117b3bfb52c6d18c04"
+        }
+    },
+    {
+        "SHA3_224_KEYLEN_EQ_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f",
+            "53616d706c65206d65737361676520666f72206b65796c656e3d626c6f636b6c656e",
+            "d8b733bcf66c644a12323d564e24dcf3fc75f231f3b67968359100c7"
+        }
+    },
+    {
+        "SHA3_224_KEYLEN_GT_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaab",
+            "53616d706c65206d65737361676520666f72206b65796c656e3e626c6f636b6c656e",
+            "078695eecc227c636ad31d063a15dd05a7e819a66ec6d8de1e193e59"
+        }
+    },
+    {
+        "SHA3_256_KEYLEN_LT_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
+            "53616d706c65206d65737361676520666f72206b65796c656e3c626c6f636b6c656e",
+            "4fe8e202c4f058e8dddc23d8c34e467343e23555e24fc2f025d598f558f67205"
+        }
+    },
+    {
+        "SHA3_256_KEYLEN_EQ_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f8081828384858687",
+            "53616d706c65206d65737361676520666f72206b65796c656e3d626c6f636b6c656e",
+            "68b94e2e538a9be4103bebb5aa016d47961d4d1aa906061313b557f8af2c3faa"
+        }
+    },
+    {
+        "SHA3_256_KEYLEN_GT_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7",
+            "53616d706c65206d65737361676520666f72206b65796c656e3e626c6f636b6c656e",
+            "9bcf2c238e235c3ce88404e813bd2f3a97185ac6f238c63d6229a00b07974258"
+        }
+    },
+    {
+        "SHA3_384_KEYLEN_LT_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f",
+            "53616d706c65206d65737361676520666f72206b65796c656e3c626c6f636b6c656e",
+            "d588a3c51f3f2d906e8298c1199aa8ff6296218127f6b38a90b6afe2c5617725bc99987f79b22a557b6520db710b7f42"
+        }
+    },
+    {
+        "SHA3_384_KEYLEN_EQ_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f6061626364656667",
+            "53616d706c65206d65737361676520666f72206b65796c656e3d626c6f636b6c656e",
+            "a27d24b592e8c8cbf6d4ce6fc5bf62d8fc98bf2d486640d9eb8099e24047837f5f3bffbe92dcce90b4ed5b1e7e44fa90"
+        }
+    },
+    {
+        "SHA3_384_KEYLEN_GT_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f9091929394959697",
+            "53616d706c65206d65737361676520666f72206b65796c656e3e626c6f636b6c656e",
+            "e5ae4c739f455279368ebf36d4f5354c95aa184c899d3870e460ebc288ef1f9470053f73f7c6da2a71bcaec38ce7d6ac"
+        }
+    },
+    {
+        "SHA3_512_KEYLEN_LT_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f",
+            "53616d706c65206d65737361676520666f72206b65796c656e3c626c6f636b6c656e",
+            "4efd629d6c71bf86162658f29943b1c308ce27cdfa6db0d9c3ce81763f9cbce5f7ebe9868031db1a8f8eb7b6b95e5c5e3f657a8996c86a2f6527e307f0213196"
+        }
+    },
+    {
+        "SHA3_512_KEYLEN_EQ_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f4041424344454647",
+            "53616d706c65206d65737361676520666f72206b65796c656e3d626c6f636b6c656e",
+            "544e257ea2a3e5ea19a590e6a24b724ce6327757723fe2751b75bf007d80f6b360744bf1b7a88ea585f9765b47911976d3191cf83c039f5ffab0d29cc9d9b6da"
+        }
+    },
+    {
+        "SHA3_512_KEYLEN_GT_B",
+        {
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f8081828384858687",
+            "53616d706c65206d65737361676520666f72206b65796c656e3e626c6f636b6c656e",
+            "5f464f5e5b7848e3885e49b2c385f0694985d0e38966242dc4a5fe3fea4b37d46b65ceced5dcf59438dd840bab22269f0ba7febdb9fcf74602a35666b2a32915"
+        }
+    }
+    
 };
 
 // clang-format on
@@ -175,6 +321,7 @@ class HmacTestFixture
     std::unique_ptr<alcp::digest::Sha224> p_sha224;
     std::unique_ptr<alcp::digest::Sha384> p_sha384;
     std::unique_ptr<alcp::digest::Sha512> p_sha512;
+    std::unique_ptr<alcp::digest::Sha3>   p_sha3;
 
   public:
     void setUp(const ParamType& params)
@@ -209,24 +356,58 @@ class HmacTestFixture
     }
     void setUpHash(std::string test_name)
     {
-        size_t      found     = test_name.find("_");
-        std::string hash_name = test_name.substr(0, found);
-        if (hash_name == "SHA256") {
-            p_sha256 = std::make_unique<alcp::digest::Sha256>();
-            p_hmac =
-                std::make_unique<alcp::mac::Hmac>(mac_info, p_sha256.get());
-        } else if (hash_name == "SHA224") {
-            p_sha224 = std::make_unique<alcp::digest::Sha224>();
-            p_hmac =
-                std::make_unique<alcp::mac::Hmac>(mac_info, p_sha224.get());
-        } else if (hash_name == "SHA384") {
-            p_sha384 = std::make_unique<alcp::digest::Sha384>();
-            p_hmac =
-                std::make_unique<alcp::mac::Hmac>(mac_info, p_sha384.get());
-        } else if (hash_name == "SHA512") {
-            p_sha512 = std::make_unique<alcp::digest::Sha512>();
-            p_hmac =
-                std::make_unique<alcp::mac::Hmac>(mac_info, p_sha512.get());
+        size_t      type_index = test_name.find("_");
+        std::string sha_type   = test_name.substr(0, type_index);
+
+        size_t      algo_index = test_name.find("_");
+        std::string hash_name =
+            test_name.substr(type_index + 1, algo_index - 1);
+
+        if (sha_type == "SHA2") {
+            if (hash_name == "256") {
+                p_sha256 = std::make_unique<alcp::digest::Sha256>();
+                p_hmac =
+                    std::make_unique<alcp::mac::Hmac>(mac_info, p_sha256.get());
+            } else if (hash_name == "224") {
+                p_sha224 = std::make_unique<alcp::digest::Sha224>();
+                p_hmac =
+                    std::make_unique<alcp::mac::Hmac>(mac_info, p_sha224.get());
+            } else if (hash_name == "384") {
+                p_sha384 = std::make_unique<alcp::digest::Sha384>();
+                p_hmac =
+                    std::make_unique<alcp::mac::Hmac>(mac_info, p_sha384.get());
+            } else if (hash_name == "512") {
+                p_sha512 = std::make_unique<alcp::digest::Sha512>();
+                p_hmac =
+                    std::make_unique<alcp::mac::Hmac>(mac_info, p_sha512.get());
+            }
+        } else if (sha_type == "SHA3") {
+            mac_info.mi_algoinfo.hmac.hmac_digest.dt_type =
+                ALC_DIGEST_TYPE_SHA3;
+            if (hash_name == "224") {
+                mac_info.mi_algoinfo.hmac.hmac_digest.dt_len =
+                    ALC_DIGEST_LEN_224;
+                mac_info.mi_algoinfo.hmac.hmac_digest.dt_mode.dm_sha3 =
+                    ALC_SHA3_224;
+            } else if (hash_name == "256") {
+                mac_info.mi_algoinfo.hmac.hmac_digest.dt_len =
+                    ALC_DIGEST_LEN_256;
+                mac_info.mi_algoinfo.hmac.hmac_digest.dt_mode.dm_sha3 =
+                    ALC_SHA3_256;
+            } else if (hash_name == "384") {
+                mac_info.mi_algoinfo.hmac.hmac_digest.dt_len =
+                    ALC_DIGEST_LEN_384;
+                mac_info.mi_algoinfo.hmac.hmac_digest.dt_mode.dm_sha3 =
+                    ALC_SHA3_384;
+            } else if (hash_name == "512") {
+                mac_info.mi_algoinfo.hmac.hmac_digest.dt_len =
+                    ALC_DIGEST_LEN_512;
+                mac_info.mi_algoinfo.hmac.hmac_digest.dt_mode.dm_sha3 =
+                    ALC_SHA3_512;
+            }
+            p_sha3 = std::make_unique<alcp::digest::Sha3>(
+                mac_info.mi_algoinfo.hmac.hmac_digest);
+            p_hmac = std::make_unique<alcp::mac::Hmac>(mac_info, p_sha3.get());
         }
     }
 };
@@ -288,7 +469,7 @@ TEST(HmacReliabilityTest, NonNullKeyNullKeyLength)
 
 TEST(HmacReliabilityTest, NullUpdate)
 {
-    auto        pos  = KATSHA256Dataset.find("SHA256_KEYLEN_EQ_B");
+    auto        pos  = KAT_ShaDataset.find("SHA2_256_KEYLEN_EQ_B");
     param_tuple data = pos->second;
 
     auto key         = parseHexStrToBin(std::get<0>(data));
@@ -317,6 +498,8 @@ TEST(HmacReliabilityTest, NullUpdate)
     };
     alcp::digest::Sha256 sha256;
     alcp::mac::Hmac      hmac(mac_info, &sha256);
+    ASSERT_EQ(hmac.getState(), VALID);
+
     hmac.update(nullptr, 0);
     ASSERT_EQ(hmac.getState(), VALID);
     hmac.update(&cipher_text[0], cipher_text.size());
@@ -375,7 +558,7 @@ TEST_P(HmacTestFixture, HMAC_UPDATE_FINALISE)
 INSTANTIATE_TEST_SUITE_P(
     HmacTest,
     HmacTestFixture,
-    testing::ValuesIn(KATSHA256Dataset),
+    testing::ValuesIn(KAT_ShaDataset),
     [](const testing::TestParamInfo<HmacTestFixture::ParamType>& info) {
         return info.param.first;
     });
