3a25139fb85cda79a1161a373a68d373e91597a7
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2023-01-20 05:15:12 +0000

    Crypto: Digest: update copyright header year

--- bench/digest/bench_main.cc
+++ bench/digest/bench_main.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2022, Advanced Micro Devices. All rights reserved.
+ * Copyright (C) 2022-2023, Advanced Micro Devices. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -42,8 +42,8 @@ main(int argc, char** argv)
 #endif
 #ifndef USE_OSSL
     if (useossl) {
-        std::cout << RED << "Error OpenSSL not found defaulting to ALCP" << RESET
-                  << std::endl;
+        std::cout << RED << "Error OpenSSL not found defaulting to ALCP"
+                  << RESET << std::endl;
     }
 #endif
     AddBenchmarks();
