f0037ae3688f2e58fee9e4bb3ede802b67cea791
Abhiram S <abhiram.s@amd.com>
2023-02-01 21:36:23 +0530

    Scripts:Env: Color support
    Signed-off-by: Abhiram S <abhiram.s@amd.com>
    
    Co-authored-by: Abhijith N Raj <abhijith.raj@amd.com>

--- scripts/setup_project.sh
+++ scripts/setup_project.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # Copyright (C) 2023, Advanced Micro Devices. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -24,6 +24,9 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+# Include the capability for colors
+source colors.sh
+
 
 # Framework functions
 check_file_exists(){
@@ -38,7 +41,9 @@ check_file_exists(){
 ensure_file_exists(){
     if [ ! -f "$1" ]
     then
+        set_fg_color $RED
         echo "File $1 does not seem to exist, aborting!"
+        reset_color
         exit 255;
     fi
 }
@@ -50,16 +55,29 @@ ensure_return_value(){
     fi
 }
 
+warn_return_value(){
+    if [ $? -ne $1 ]
+    then
+        set_fg_color $RED
+        echo "Above operation might have FAILED!"
+        reset_color
+    fi
+}
+
 ensure_linking(){
     ensure_file_exists "$1"
     check_file_exists  "$2"
     if [ $? -eq 0 ]
     then
+        set_fg_color $GREEN
         echo "Creating a symbolic link between $1 $2"
         ln -s "$1" "$2"
+        reset_color
         ensure_return_value 0
     else
+        set_fg_color $YELLOW
         echo "$2 seems to exist, if its a copy, please ensure its upto date with $1"
+        reset_color
     fi
 }
 
@@ -70,7 +88,9 @@ check_git_dir(){
 
     if [ "$out" != "true" ]
     then
+        set_fg_color $RED
         echo "Not inside git directory!"
+        reset_color
         exit 255;
     fi
 }
@@ -81,9 +101,13 @@ check_repo_is_alcp(){
     cat $GIT_WORKING_DIR/CMakeLists.txt | grep -i "PROJECT" | grep -i "alcp" > /dev/null
     if [ $? -eq 0 ]
     then
+        set_fg_color $GREEN
         echo "Project ALCP Detected"
+        reset_color
     else
+        set_fg_color $RED
         echo "Project is not ALCP, cannot continue!"
+        reset_color
         exit 255;
     fi
 }
@@ -100,11 +124,36 @@ ensure_git_hooks(){
     ensure_git_precommit_hook;
 }
 
+clean(){
+    # TODO: Needs argument parser
+    if [ "$1" = "-c" ]
+    then
+        set_fg_color $YELLOW
+        echo "Removing ""$GIT_WORKING_DIR/.git/hooks/pre-commit"
+        reset_color
+        rm "$GIT_WORKING_DIR/.git/hooks/pre-commit" 2>&1 > /dev/null
+        warn_return_value 0
+        set_fg_color $YELLOW
+        echo "Removing ""$GIT_WORKING_DIR/.clang-format"
+        reset_color
+        rm "$GIT_WORKING_DIR/.clang-format" 2>&1 > /dev/null
+        warn_return_value 0
+
+        set_fg_color $GREEN
+        echo "Done cleaning"
+        reset_color
+        exit 0
+    fi
+}
+
 check_git_dir;
 
 GIT_WORKING_DIR=$(git rev-parse --show-toplevel 2>&1)
+set_fg_color $YELLOW
 echo "Found Working Git Directory as $GIT_WORKING_DIR"
+reset_color
 
+clean $@;
 check_repo_is_alcp;
 ensure_clang_format;
 ensure_git_hooks;
