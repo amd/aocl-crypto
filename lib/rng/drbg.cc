7cb52453adb2dce61f7990d2bf9c5ac713ec5fb3
Abhiram S <abhiram.s@amd.com>
2023-02-07 20:54:00 +0530

    lib:RNG:DRBG: DRBG wrapper split export def into .cc
    Signed-off-by: Abhiram S <abhiram.s@amd.com>
    
    Co-authored-by: Abhijith N Raj <abhijith.raj@amd.com>

--- lib/rng/drbg.cc
+++ lib/rng/drbg.cc
@@ -25,3 +25,104 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  */
+
+#include "rng/drbg.hh"
+#include "alcp/base.hh"
+#include "rng.hh"
+#include <memory>
+
+namespace alcp { namespace random_number {
+
+    // FIXME: Predicition resistance is to be added
+    Status Drbg::initialize(int                 security_strength,
+                            std::vector<Uint8>& personalization_string)
+    {
+        Status s = StatusOk();
+#if 0
+            /*
+                FIXME: Implement Security Strength
+                getCurrMinSecurity() -> Implemented in HMAC_Drbg
+                Calling the above function returns the min security strength.
+            */
+            if (security_strength < get_curr_min_security()) {
+                // Bail out
+                // Return error here
+                s = alcp::base::InternalError(
+                    "ERROR: Requested algorithm does not meet min requirements "
+                    "for security level specified.");
+                return s;
+            }
+#endif
+        std::vector<Uint8> entropy_input(128);
+        std::vector<Uint8> nonce(128);
+
+        s = m_entropy_in->randomize(&entropy_input[0], entropy_input.size());
+        if (!s.ok()) {
+            return s;
+        }
+
+        s = m_entropy_in->randomize(&nonce[0], nonce.size());
+        if (!s.ok()) {
+            return s;
+        }
+
+        Instantiate(entropy_input, nonce, personalization_string);
+        return s;
+    }
+
+    // FIXME: Predicition resistance is to be added
+    Status Drbg::randomize(Uint8               output[],
+                           size_t              length,
+                           int                 security_strength,
+                           std::vector<Uint8>& additional_input)
+    {
+        Status s = StatusOk();
+#if 0
+            // TODO: Enable after implementing
+            /*
+                FIXME: Implement Max Generate Bits
+                get_max_generatable_bits() -> Implemented in HMAC_Drbg
+                Calling the above function returns the current security strength.
+            */
+            if (length > get_max_generatable_bits()) {
+                s = InternalError(
+                    "ERROR: Impossible amount of bits to generate per call");
+                return s;
+            }
+            /*
+                FIXME: Implement Security Strength
+                get_curr_security_strength() -> Implemented in HMAC_Drbg
+                Calling the above function returns the current security strength.
+            */
+            if (security_strength >= get_curr_security_strength()) {
+                // Bail out
+                // Return error here
+                s = alcp::base::InternalError(
+                    "ERROR: Requested algorithm does not meet min requirements "
+                    "for security level specified.");
+                return s;
+            }
+            /*
+                FIXME: Implement Max Additional Input Length
+                get_max_generatable_bits() -> Implemented in HMAC_Drbg
+                Calling the above function returns the current security strength.
+            */
+            if(additional_input.size()> get_max_add_input_length()){
+                s = InternalError(
+                    "ERROR: Additional Input too large");
+                return s;
+            }
+            // FIXME: Handle Predicition Resistance Request
+            // FIXME: Handle reseed required flag
+#endif
+        Generate(&additional_input[0], additional_input.size(), output, length);
+        return s;
+    }
+
+    Status Drbg::randomize(Uint8 output[], size_t length)
+    {
+        std::vector<Uint8> add = std::vector<Uint8>(0);
+        return randomize(output, length, 512, add);
+    }
+
+}} // namespace alcp::random_number
