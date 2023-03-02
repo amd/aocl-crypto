c99f816bcbe986892b491a143e098edf92cd5a0a
Abhiram S <abhiram.s@amd.com>
2023-03-02 15:10:12 +0530

    lib:arch: Initialization of variables,Copyright FIXES

--- lib/arch/zen4/vaes_avx512_core.hh
+++ lib/arch/zen4/vaes_avx512_core.hh
@@ -1,3 +1,31 @@
+/*
+ * Copyright (C) 2022-2023, Advanced Micro Devices. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 #pragma once
 
 #include <immintrin.h>
@@ -6,22 +34,22 @@
 
 namespace alcp::cipher { namespace vaes512 {
 
-    static inline void alcp_load_key_zmm(const __m128i* pkey128,
-                                         __m512i&       key_512_0,
-                                         __m512i&       key_512_1,
-                                         __m512i&       key_512_2,
-                                         __m512i&       key_512_3,
-                                         __m512i&       key_512_4,
-                                         __m512i&       key_512_5,
-                                         __m512i&       key_512_6,
-                                         __m512i&       key_512_7,
-                                         __m512i&       key_512_8,
-                                         __m512i&       key_512_9,
-                                         __m512i&       key_512_10,
-                                         __m512i&       key_512_11,
-                                         __m512i&       key_512_12,
-                                         __m512i&       key_512_13,
-                                         __m512i&       key_512_14)
+    static inline void alcp_load_key_zmm(const __m128i pkey128[],
+                                         __m512i&      key_512_0,
+                                         __m512i&      key_512_1,
+                                         __m512i&      key_512_2,
+                                         __m512i&      key_512_3,
+                                         __m512i&      key_512_4,
+                                         __m512i&      key_512_5,
+                                         __m512i&      key_512_6,
+                                         __m512i&      key_512_7,
+                                         __m512i&      key_512_8,
+                                         __m512i&      key_512_9,
+                                         __m512i&      key_512_10,
+                                         __m512i&      key_512_11,
+                                         __m512i&      key_512_12,
+                                         __m512i&      key_512_13,
+                                         __m512i&      key_512_14)
     {
         key_512_0  = _mm512_broadcast_i64x2(*pkey128);
         key_512_1  = _mm512_broadcast_i64x2(*(pkey128 + 1));
