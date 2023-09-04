cbf099d1df0ece8bff8bd63a46558bb760a93c58
ashanand <ashish.anand@amd.com>
2023-09-04 12:28:55 +0000

    Optimize AMS52 for RSA2048

--- lib/arch/zen4/rsa.cc
+++ lib/arch/zen4/rsa.cc
@@ -140,100 +140,326 @@ namespace alcp::rsa { namespace zen4 {
         }
     }
 
-    static inline void AMS2048(Uint64*       res,
-                               const Uint64* first,
-                               const Uint64* mod,
-                               Uint64        k0)
+    static inline void LoopInternalStage1(__m512i       res_reg[5],
+                                          __m512i       first_reg[5],
+                                          __m512i       mod_reg[5],
+                                          const Uint64* first,
+                                          Uint64        k0)
     {
-        __m512i first_reg_0;
-        __m512i first_reg_1;
-        __m512i first_reg_2;
-        __m512i first_reg_3;
-        __m512i first_reg_4;
-
-        __m512i mod_reg_0;
-        __m512i mod_reg_1;
-        __m512i mod_reg_2;
-        __m512i mod_reg_3;
-        __m512i mod_reg_4;
-
-        __m512i res_reg_0{};
-        __m512i res_reg_1{};
-        __m512i res_reg_2{};
-        __m512i res_reg_3{};
-        __m512i res_reg_4{};
-
-        first_reg_0 = _mm512_loadu_si512(first);
-        first_reg_1 = _mm512_loadu_si512(first + 8);
-        first_reg_2 = _mm512_loadu_si512(first + 16);
-        first_reg_3 = _mm512_loadu_si512(first + 24);
-        first_reg_4 = _mm512_loadu_si512(first + 32);
-
-        mod_reg_0 = _mm512_loadu_si512(mod);
-        mod_reg_1 = _mm512_loadu_si512(mod + 8);
-        mod_reg_2 = _mm512_loadu_si512(mod + 16);
-        mod_reg_3 = _mm512_loadu_si512(mod + 24);
-        mod_reg_4 = _mm512_loadu_si512(mod + 32);
-
         const __m512i zero{};
+        for (Uint64 j = 0; j < 8; j++) {
 
-        for (Uint64 i = 0; i < num_digit; i++) {
-            __m512i second_reg = _mm512_set1_epi64(first[i]);
+            __m512i second_reg = _mm512_set1_epi64(first[j]);
 
-            res_reg_0 =
-                _mm512_madd52lo_epu64(res_reg_0, first_reg_0, second_reg);
-            res_reg_1 =
-                _mm512_madd52lo_epu64(res_reg_1, first_reg_1, second_reg);
-            res_reg_2 =
-                _mm512_madd52lo_epu64(res_reg_2, first_reg_2, second_reg);
-            res_reg_3 =
-                _mm512_madd52lo_epu64(res_reg_3, first_reg_3, second_reg);
-            res_reg_4 =
-                _mm512_madd52lo_epu64(res_reg_4, first_reg_4, second_reg);
+            res_reg[0] =
+                _mm512_madd52lo_epu64(res_reg[0], first_reg[0], second_reg);
 
-            Uint64 x0 = _mm_cvtsi128_si64(_mm512_castsi512_si128(res_reg_0));
+            for (Uint64 z = 1; z < 5; z++) {
+                __m512i temp =
+                    _mm512_madd52lo_epu64(zero, first_reg[z], second_reg);
+
+                temp       = _mm512_slli_epi64(temp, 1);
+                res_reg[z] = _mm512_add_epi64(temp, res_reg[z]);
+            }
+
+            Uint64 x0 = _mm_cvtsi128_si64(_mm512_castsi512_si128(res_reg[0]));
 
             Uint64 y0 = (k0 * (x0 & 0xfffffffffffff)) & 0xfffffffffffff;
 
             __m512i y_reg = _mm512_set1_epi64(y0);
 
-            res_reg_0 = _mm512_madd52lo_epu64(res_reg_0, mod_reg_0, y_reg);
-            res_reg_1 = _mm512_madd52lo_epu64(res_reg_1, mod_reg_1, y_reg);
-            res_reg_2 = _mm512_madd52lo_epu64(res_reg_2, mod_reg_2, y_reg);
-            res_reg_3 = _mm512_madd52lo_epu64(res_reg_3, mod_reg_3, y_reg);
-            res_reg_4 = _mm512_madd52lo_epu64(res_reg_4, mod_reg_4, y_reg);
+            res_reg[0] = _mm512_madd52lo_epu64(res_reg[0], mod_reg[0], y_reg);
+            res_reg[1] = _mm512_madd52lo_epu64(res_reg[1], mod_reg[1], y_reg);
+            res_reg[2] = _mm512_madd52lo_epu64(res_reg[2], mod_reg[2], y_reg);
+            res_reg[3] = _mm512_madd52lo_epu64(res_reg[3], mod_reg[3], y_reg);
+            res_reg[4] = _mm512_madd52lo_epu64(res_reg[4], mod_reg[4], y_reg);
 
-            __m512i carry = _mm512_maskz_srli_epi64(1, res_reg_0, 52);
-            res_reg_0     = _mm512_alignr_epi64(res_reg_1, res_reg_0, 1);
-            res_reg_0     = _mm512_add_epi64(res_reg_0, carry);
-            res_reg_1     = _mm512_alignr_epi64(res_reg_2, res_reg_1, 1);
-            res_reg_2     = _mm512_alignr_epi64(res_reg_3, res_reg_2, 1);
-            res_reg_3     = _mm512_alignr_epi64(res_reg_4, res_reg_3, 1);
-            res_reg_4     = _mm512_alignr_epi64(zero, res_reg_4, 1);
+            __m512i carry = _mm512_maskz_srli_epi64(1, res_reg[0], 52);
+            res_reg[0]    = _mm512_alignr_epi64(res_reg[1], res_reg[0], 1);
+            res_reg[0]    = _mm512_add_epi64(res_reg[0], carry);
+            res_reg[1]    = _mm512_alignr_epi64(res_reg[2], res_reg[1], 1);
+            res_reg[2]    = _mm512_alignr_epi64(res_reg[3], res_reg[2], 1);
+            res_reg[3]    = _mm512_alignr_epi64(res_reg[4], res_reg[3], 1);
+            res_reg[4]    = _mm512_alignr_epi64(zero, res_reg[4], 1);
 
-            res_reg_0 =
-                _mm512_madd52hi_epu64(res_reg_0, first_reg_0, second_reg);
-            res_reg_1 =
-                _mm512_madd52hi_epu64(res_reg_1, first_reg_1, second_reg);
-            res_reg_2 =
-                _mm512_madd52hi_epu64(res_reg_2, first_reg_2, second_reg);
-            res_reg_3 =
-                _mm512_madd52hi_epu64(res_reg_3, first_reg_3, second_reg);
-            res_reg_4 =
-                _mm512_madd52hi_epu64(res_reg_4, first_reg_4, second_reg);
+            res_reg[0] =
+                _mm512_madd52hi_epu64(res_reg[0], first_reg[0], second_reg);
 
-            res_reg_0 = _mm512_madd52hi_epu64(res_reg_0, mod_reg_0, y_reg);
-            res_reg_1 = _mm512_madd52hi_epu64(res_reg_1, mod_reg_1, y_reg);
-            res_reg_2 = _mm512_madd52hi_epu64(res_reg_2, mod_reg_2, y_reg);
-            res_reg_3 = _mm512_madd52hi_epu64(res_reg_3, mod_reg_3, y_reg);
-            res_reg_4 = _mm512_madd52hi_epu64(res_reg_4, mod_reg_4, y_reg);
+            for (Uint64 z = 1; z < 5; z++) {
+                __m512i temp =
+                    _mm512_madd52hi_epu64(zero, first_reg[z], second_reg);
+
+                temp       = _mm512_slli_epi64(temp, 1);
+                res_reg[z] = _mm512_add_epi64(temp, res_reg[z]);
+            }
+
+            res_reg[0] = _mm512_madd52hi_epu64(res_reg[0], mod_reg[0], y_reg);
+            res_reg[1] = _mm512_madd52hi_epu64(res_reg[1], mod_reg[1], y_reg);
+            res_reg[2] = _mm512_madd52hi_epu64(res_reg[2], mod_reg[2], y_reg);
+            res_reg[3] = _mm512_madd52hi_epu64(res_reg[3], mod_reg[3], y_reg);
+            res_reg[4] = _mm512_madd52hi_epu64(res_reg[4], mod_reg[4], y_reg);
         }
+    }
 
-        _mm512_storeu_si512(res, res_reg_0);
-        _mm512_storeu_si512(res + 8, res_reg_1);
-        _mm512_storeu_si512(res + 16, res_reg_2);
-        _mm512_storeu_si512(res + 24, res_reg_3);
-        _mm512_storeu_si512(res + 32, res_reg_4);
+    static inline void LoopInternalStage2(__m512i       res_reg[5],
+                                          __m512i       first_reg[5],
+                                          __m512i       mod_reg[5],
+                                          const Uint64* first,
+                                          Uint64        k0)
+    {
+        const __m512i zero{};
+        for (Uint64 j = 0; j < 8; j++) {
+
+            __m512i second_reg = _mm512_set1_epi64(first[j]);
+
+            res_reg[1] =
+                _mm512_madd52lo_epu64(res_reg[1], first_reg[1], second_reg);
+
+            for (Uint64 z = 2; z < 5; z++) {
+                __m512i temp =
+                    _mm512_madd52lo_epu64(zero, first_reg[z], second_reg);
+
+                temp       = _mm512_slli_epi64(temp, 1);
+                res_reg[z] = _mm512_add_epi64(temp, res_reg[z]);
+            }
+
+            Uint64 x0 = _mm_cvtsi128_si64(_mm512_castsi512_si128(res_reg[0]));
+
+            Uint64 y0 = (k0 * (x0 & 0xfffffffffffff)) & 0xfffffffffffff;
+
+            __m512i y_reg = _mm512_set1_epi64(y0);
+
+            res_reg[0] = _mm512_madd52lo_epu64(res_reg[0], mod_reg[0], y_reg);
+            res_reg[1] = _mm512_madd52lo_epu64(res_reg[1], mod_reg[1], y_reg);
+            res_reg[2] = _mm512_madd52lo_epu64(res_reg[2], mod_reg[2], y_reg);
+            res_reg[3] = _mm512_madd52lo_epu64(res_reg[3], mod_reg[3], y_reg);
+            res_reg[4] = _mm512_madd52lo_epu64(res_reg[4], mod_reg[4], y_reg);
+
+            __m512i carry = _mm512_maskz_srli_epi64(1, res_reg[0], 52);
+            res_reg[0]    = _mm512_alignr_epi64(res_reg[1], res_reg[0], 1);
+            res_reg[0]    = _mm512_add_epi64(res_reg[0], carry);
+            res_reg[1]    = _mm512_alignr_epi64(res_reg[2], res_reg[1], 1);
+            res_reg[2]    = _mm512_alignr_epi64(res_reg[3], res_reg[2], 1);
+            res_reg[3]    = _mm512_alignr_epi64(res_reg[4], res_reg[3], 1);
+            res_reg[4]    = _mm512_alignr_epi64(zero, res_reg[4], 1);
+
+            res_reg[1] =
+                _mm512_madd52hi_epu64(res_reg[1], first_reg[1], second_reg);
+
+            for (Uint64 z = 2; z < 5; z++) {
+                __m512i temp =
+                    _mm512_madd52hi_epu64(zero, first_reg[z], second_reg);
+
+                temp       = _mm512_slli_epi64(temp, 1);
+                res_reg[z] = _mm512_add_epi64(temp, res_reg[z]);
+            }
+
+            res_reg[0] = _mm512_madd52hi_epu64(res_reg[0], mod_reg[0], y_reg);
+            res_reg[1] = _mm512_madd52hi_epu64(res_reg[1], mod_reg[1], y_reg);
+            res_reg[2] = _mm512_madd52hi_epu64(res_reg[2], mod_reg[2], y_reg);
+            res_reg[3] = _mm512_madd52hi_epu64(res_reg[3], mod_reg[3], y_reg);
+            res_reg[4] = _mm512_madd52hi_epu64(res_reg[4], mod_reg[4], y_reg);
+        }
+    }
+    static inline void LoopInternalStage3(__m512i       res_reg[5],
+                                          __m512i       first_reg[5],
+                                          __m512i       mod_reg[5],
+                                          const Uint64* first,
+                                          Uint64        k0)
+    {
+        const __m512i zero{};
+        for (Uint64 j = 0; j < 8; j++) {
+
+            __m512i second_reg = _mm512_set1_epi64(first[j]);
+
+            res_reg[2] =
+                _mm512_madd52lo_epu64(res_reg[2], first_reg[2], second_reg);
+
+            for (Uint64 z = 3; z < 5; z++) {
+                __m512i temp =
+                    _mm512_madd52lo_epu64(zero, first_reg[z], second_reg);
+
+                temp       = _mm512_slli_epi64(temp, 1);
+                res_reg[z] = _mm512_add_epi64(temp, res_reg[z]);
+            }
+
+            Uint64 x0 = _mm_cvtsi128_si64(_mm512_castsi512_si128(res_reg[0]));
+
+            Uint64 y0 = (k0 * (x0 & 0xfffffffffffff)) & 0xfffffffffffff;
+
+            __m512i y_reg = _mm512_set1_epi64(y0);
+
+            res_reg[0] = _mm512_madd52lo_epu64(res_reg[0], mod_reg[0], y_reg);
+            res_reg[1] = _mm512_madd52lo_epu64(res_reg[1], mod_reg[1], y_reg);
+            res_reg[2] = _mm512_madd52lo_epu64(res_reg[2], mod_reg[2], y_reg);
+            res_reg[3] = _mm512_madd52lo_epu64(res_reg[3], mod_reg[3], y_reg);
+            res_reg[4] = _mm512_madd52lo_epu64(res_reg[4], mod_reg[4], y_reg);
+
+            __m512i carry = _mm512_maskz_srli_epi64(1, res_reg[0], 52);
+            res_reg[0]    = _mm512_alignr_epi64(res_reg[1], res_reg[0], 1);
+            res_reg[0]    = _mm512_add_epi64(res_reg[0], carry);
+            res_reg[1]    = _mm512_alignr_epi64(res_reg[2], res_reg[1], 1);
+            res_reg[2]    = _mm512_alignr_epi64(res_reg[3], res_reg[2], 1);
+            res_reg[3]    = _mm512_alignr_epi64(res_reg[4], res_reg[3], 1);
+            res_reg[4]    = _mm512_alignr_epi64(zero, res_reg[4], 1);
+
+            res_reg[2] =
+                _mm512_madd52hi_epu64(res_reg[2], first_reg[2], second_reg);
+
+            for (Uint64 z = 3; z < 5; z++) {
+                __m512i temp =
+                    _mm512_madd52hi_epu64(zero, first_reg[z], second_reg);
+
+                temp       = _mm512_slli_epi64(temp, 1);
+                res_reg[z] = _mm512_add_epi64(temp, res_reg[z]);
+            }
+
+            res_reg[0] = _mm512_madd52hi_epu64(res_reg[0], mod_reg[0], y_reg);
+            res_reg[1] = _mm512_madd52hi_epu64(res_reg[1], mod_reg[1], y_reg);
+            res_reg[2] = _mm512_madd52hi_epu64(res_reg[2], mod_reg[2], y_reg);
+            res_reg[3] = _mm512_madd52hi_epu64(res_reg[3], mod_reg[3], y_reg);
+            res_reg[4] = _mm512_madd52hi_epu64(res_reg[4], mod_reg[4], y_reg);
+        }
+    }
+    static inline void LoopInternalStage4(__m512i       res_reg[5],
+                                          __m512i       first_reg[5],
+                                          __m512i       mod_reg[5],
+                                          const Uint64* first,
+                                          Uint64        k0)
+    {
+        const __m512i zero{};
+        for (Uint64 j = 0; j < 8; j++) {
+
+            __m512i second_reg = _mm512_set1_epi64(first[j]);
+
+            res_reg[3] =
+                _mm512_madd52lo_epu64(res_reg[3], first_reg[3], second_reg);
+
+            for (Uint64 z = 4; z < 5; z++) {
+                __m512i temp =
+                    _mm512_madd52lo_epu64(zero, first_reg[z], second_reg);
+
+                temp       = _mm512_slli_epi64(temp, 1);
+                res_reg[z] = _mm512_add_epi64(temp, res_reg[z]);
+            }
+
+            Uint64 x0 = _mm_cvtsi128_si64(_mm512_castsi512_si128(res_reg[0]));
+
+            Uint64 y0 = (k0 * (x0 & 0xfffffffffffff)) & 0xfffffffffffff;
+
+            __m512i y_reg = _mm512_set1_epi64(y0);
+
+            res_reg[0] = _mm512_madd52lo_epu64(res_reg[0], mod_reg[0], y_reg);
+            res_reg[1] = _mm512_madd52lo_epu64(res_reg[1], mod_reg[1], y_reg);
+            res_reg[2] = _mm512_madd52lo_epu64(res_reg[2], mod_reg[2], y_reg);
+            res_reg[3] = _mm512_madd52lo_epu64(res_reg[3], mod_reg[3], y_reg);
+            res_reg[4] = _mm512_madd52lo_epu64(res_reg[4], mod_reg[4], y_reg);
+
+            __m512i carry = _mm512_maskz_srli_epi64(1, res_reg[0], 52);
+            res_reg[0]    = _mm512_alignr_epi64(res_reg[1], res_reg[0], 1);
+            res_reg[0]    = _mm512_add_epi64(res_reg[0], carry);
+            res_reg[1]    = _mm512_alignr_epi64(res_reg[2], res_reg[1], 1);
+            res_reg[2]    = _mm512_alignr_epi64(res_reg[3], res_reg[2], 1);
+            res_reg[3]    = _mm512_alignr_epi64(res_reg[4], res_reg[3], 1);
+            res_reg[4]    = _mm512_alignr_epi64(zero, res_reg[4], 1);
+
+            res_reg[3] =
+                _mm512_madd52hi_epu64(res_reg[3], first_reg[3], second_reg);
+
+            for (Uint64 z = 4; z < 5; z++) {
+                __m512i temp =
+                    _mm512_madd52hi_epu64(zero, first_reg[z], second_reg);
+
+                temp       = _mm512_slli_epi64(temp, 1);
+                res_reg[z] = _mm512_add_epi64(temp, res_reg[z]);
+            }
+
+            res_reg[0] = _mm512_madd52hi_epu64(res_reg[0], mod_reg[0], y_reg);
+            res_reg[1] = _mm512_madd52hi_epu64(res_reg[1], mod_reg[1], y_reg);
+            res_reg[2] = _mm512_madd52hi_epu64(res_reg[2], mod_reg[2], y_reg);
+            res_reg[3] = _mm512_madd52hi_epu64(res_reg[3], mod_reg[3], y_reg);
+            res_reg[4] = _mm512_madd52hi_epu64(res_reg[4], mod_reg[4], y_reg);
+        }
+    }
+
+    static inline void LoopInternalStage5(__m512i       res_reg[5],
+                                          __m512i       first_reg[5],
+                                          __m512i       mod_reg[5],
+                                          const Uint64* first,
+                                          Uint64        k0)
+    {
+        const __m512i zero{};
+        for (Uint64 j = 0; j < 8; j++) {
+
+            __m512i second_reg = _mm512_set1_epi64(first[j]);
+
+            res_reg[4] =
+                _mm512_madd52lo_epu64(res_reg[4], first_reg[4], second_reg);
+
+            Uint64 x0 = _mm_cvtsi128_si64(_mm512_castsi512_si128(res_reg[0]));
+
+            Uint64 y0 = (k0 * (x0 & 0xfffffffffffff)) & 0xfffffffffffff;
+
+            __m512i y_reg = _mm512_set1_epi64(y0);
+
+            res_reg[0] = _mm512_madd52lo_epu64(res_reg[0], mod_reg[0], y_reg);
+            res_reg[1] = _mm512_madd52lo_epu64(res_reg[1], mod_reg[1], y_reg);
+            res_reg[2] = _mm512_madd52lo_epu64(res_reg[2], mod_reg[2], y_reg);
+            res_reg[3] = _mm512_madd52lo_epu64(res_reg[3], mod_reg[3], y_reg);
+            res_reg[4] = _mm512_madd52lo_epu64(res_reg[4], mod_reg[4], y_reg);
+
+            __m512i carry = _mm512_maskz_srli_epi64(1, res_reg[0], 52);
+            res_reg[0]    = _mm512_alignr_epi64(res_reg[1], res_reg[0], 1);
+            res_reg[0]    = _mm512_add_epi64(res_reg[0], carry);
+            res_reg[1]    = _mm512_alignr_epi64(res_reg[2], res_reg[1], 1);
+            res_reg[2]    = _mm512_alignr_epi64(res_reg[3], res_reg[2], 1);
+            res_reg[3]    = _mm512_alignr_epi64(res_reg[4], res_reg[3], 1);
+            res_reg[4]    = _mm512_alignr_epi64(zero, res_reg[4], 1);
+
+            res_reg[4] =
+                _mm512_madd52hi_epu64(res_reg[4], first_reg[4], second_reg);
+
+            res_reg[0] = _mm512_madd52hi_epu64(res_reg[0], mod_reg[0], y_reg);
+            res_reg[1] = _mm512_madd52hi_epu64(res_reg[1], mod_reg[1], y_reg);
+            res_reg[2] = _mm512_madd52hi_epu64(res_reg[2], mod_reg[2], y_reg);
+            res_reg[3] = _mm512_madd52hi_epu64(res_reg[3], mod_reg[3], y_reg);
+            res_reg[4] = _mm512_madd52hi_epu64(res_reg[4], mod_reg[4], y_reg);
+        }
+    }
+
+    static inline void AMS2048(Uint64*       res,
+                               const Uint64* first,
+                               const Uint64* mod,
+                               Uint64        k0)
+    {
+        __m512i first_reg[5];
+        __m512i mod_reg[5];
+
+        __m512i res_reg[5]{};
+
+        first_reg[0] = _mm512_loadu_si512(first);
+        first_reg[1] = _mm512_loadu_si512(first + 8);
+        first_reg[2] = _mm512_loadu_si512(first + 16);
+        first_reg[3] = _mm512_loadu_si512(first + 24);
+        first_reg[4] = _mm512_loadu_si512(first + 32);
+
+        mod_reg[0] = _mm512_loadu_si512(mod);
+        mod_reg[1] = _mm512_loadu_si512(mod + 8);
+        mod_reg[2] = _mm512_loadu_si512(mod + 16);
+        mod_reg[3] = _mm512_loadu_si512(mod + 24);
+        mod_reg[4] = _mm512_loadu_si512(mod + 32);
+
+        LoopInternalStage1(res_reg, first_reg, mod_reg, first, k0);
+        LoopInternalStage2(res_reg, first_reg, mod_reg, first + 8, k0);
+        LoopInternalStage3(res_reg, first_reg, mod_reg, first + 16, k0);
+        LoopInternalStage4(res_reg, first_reg, mod_reg, first + 24, k0);
+        LoopInternalStage5(res_reg, first_reg, mod_reg, first + 32, k0);
+
+        _mm512_storeu_si512(res, res_reg[0]);
+        _mm512_storeu_si512(res + 8, res_reg[1]);
+        _mm512_storeu_si512(res + 16, res_reg[2]);
+        _mm512_storeu_si512(res + 24, res_reg[3]);
+        _mm512_storeu_si512(res + 32, res_reg[4]);
 
         Uint64 carry = 0;
         // convert from redundant radix 2^52 to radix 2^52
