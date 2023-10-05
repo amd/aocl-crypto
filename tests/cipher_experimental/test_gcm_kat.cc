efcbd9b005a2228f897113aadfee7e36b13a1ff2
Abhiram S <abhiram.s@amd.com>
2023-10-05 09:58:52 +0000

    Testing:Cipher:GCM:EXP: Rewrite KAT with Factory
    Signed-off-by: Abhiram S <abhiram.s@amd.com>
    Co-authored-by: Abhijith N Raj <abhijith.raj@amd.com>

--- tests/cipher_experimental/test_gcm_kat.cc
+++ tests/cipher_experimental/test_gcm_kat.cc
@@ -30,64 +30,21 @@
 #include <map>
 #include <memory>
 
-#include "cipher_experimental/alc_cipher_gcm.hh"
+#include "cipher_experimental/factory.hh"
+#include "common/experimental/gtest_essentials.hh"
+
 #include "csv.hh"
 #include "utils.hh"
 
-using test_data_t = std::map<const std::string, std::vector<Uint8>>;
+// #include "gtest"
 
 using alcp::testing::Csv;
-using alcp::testing::cipher::ITestCipher;
-using namespace alcp::testing::cipher::gcm;
 
-// experimental code
-#if 0
-class GcmParameterizedTestFixture : public ::testing::TestWithParam<test_data_t>
-{
-
-    void SetUp() override
-    {
-        test_data_t kat_vectors = GetParam();
-        std::cout << "Key"
-                  << alcp::testing::utils::parseBytesToHexStr(
-                         &kat_vectors.at("Key")[0], kat_vectors["Key"].size())
-                  << std::endl;
-    }
-    void TearDown() override {}
-};
-
-TEST_P(GcmParameterizedTestFixture, GCM) {}
-
-void
-GenerateTestsFromCsv(const std::string filename)
-{
-    Csv csv(filename);
-
-    std::vector<Uint8> datasetPlainText  = csv.getVect("PLAINTEXT");
-    std::vector<Uint8> datasetInitvector = csv.getVect("INTVECT");
-    std::vector<Uint8> datasetKey        = csv.getVect("KEY");
-    std::vector<Uint8> datasetCipherText = csv.getVect("CIPHERTEXT");
-    std::vector<Uint8> datasetAddData    = csv.getVect("ADDITIONAL_DATA");
-    std::vector<Uint8> datasetTag        = csv.getVect("TAG");
-
-    test_data_t datasetMap;
-    datasetMap["PT"]   = datasetPlainText;
-    datasetMap["INIT"] = datasetInitvector;
-    datasetMap["KEY"]  = datasetKey;
-    datasetMap["CT"]   = datasetCipherText;
-    datasetMap["ADD"]  = datasetAddData;
-    datasetMap["TAG"]  = datasetTag;
-
-    ::testing::RegisterTest("GcmParameterizedTestFixture",
-                            "GCM",
-                            filename.c_str(),
-                            ::testing::internal::MapGenerator < datasetMap);
-}
-#endif
+namespace alcp::testing::cipher::gcm {
 
 template<bool encryptor>
 void
-GcmKat(const std::string filename)
+GcmKat(const std::string filename, std::unique_ptr<ITestCipher> iTestCipher)
 {
     Csv csv(filename);
 
@@ -103,9 +60,8 @@ GcmKat(const std::string filename)
         // Output Buffers
         std::vector<Uint8> output(
             std::max(datasetPlainText.size(), datasetCipherText.size()), 1);
-        std::vector<Uint8>           tagbuff(datasetTag.size());
-        std::unique_ptr<ITestCipher> iTestCipher =
-            std::make_unique<AlcpGcmCipher<encryptor>>();
+        std::vector<Uint8> tagbuff(datasetTag.size());
+
         alc_test_gcm_init_data_t dataInit;
         dataInit.m_iv      = &datasetInitvector[0];
         dataInit.m_iv_len  = datasetInitvector.size();
@@ -131,7 +87,9 @@ GcmKat(const std::string filename)
         dataFinalize.m_tag_expected = &datasetTag[0];
         dataFinalize.m_tag_len      = datasetTag.size();
         dataFinalize.m_tag          = &tagbuff[0];
-        dataFinalize.verified       = false;
+        dataFinalize.m_out    = dataUpdate.m_output; // If needed for padding
+        dataFinalize.m_pt_len = datasetPlainText.size();
+        dataFinalize.verified = false;
 
         ASSERT_TRUE(iTestCipher->init(&dataInit));
         ASSERT_TRUE(iTestCipher->update(&dataUpdate));
@@ -147,21 +105,106 @@ GcmKat(const std::string filename)
     }
 }
 
-TEST(KnownAnswerTest, GCM_Encrypt_experimental)
+// Reference :
+// https://google.github.io/googletest/advanced.html#registering-tests-programmatically
+class MyFixture : public ::testing::Test
 {
-    GcmKat<true>("dataset_gcm.csv");
-}
+  public:
+    // All of these optional, just like in regular macro usage.
+    static void SetUpTestSuite() {}
+    static void TearDownTestSuite() {}
+    void        SetUp() override {}
+    void        TearDown() override {}
+};
 
-TEST(KnownAnswerTest, GCM_Decrypt_experimental)
+template<bool encryptor>
+class MyTest : public MyFixture
 {
-    GcmKat<false>("dataset_gcm.csv");
+  public:
+    explicit MyTest(LibrarySelect data)
+        : data_(data)
+    {
+        iTestCipher = GcmCipherFactory<encryptor>(data);
+    }
+    void TestBody() override
+    {
+        if (iTestCipher == nullptr) {
+            FAIL() << "Requested Library object could not be initialized!";
+        }
+        GcmKat<encryptor>("dataset_gcm.csv", std::move(iTestCipher));
+    }
+
+  private:
+    LibrarySelect                data_;
+    std::unique_ptr<ITestCipher> iTestCipher;
+};
+
+template<bool encryptor>
+void
+RegisterMyTests(std::string         testSuiteName,
+                std::string         testCaseName,
+                const LibrarySelect value)
+{
+    ::testing::RegisterTest(
+        testSuiteName.c_str(),
+        testCaseName.c_str(),
+        nullptr,
+        nullptr,
+        __FILE__,
+        __LINE__,
+        // Important to use the fixture type as the return type here.
+        [=]() -> MyFixture* { return new MyTest<encryptor>(value); });
 }
+} // namespace alcp::testing::cipher::gcm
+
+using namespace alcp::testing::cipher::gcm;
+using alcp::testing::cipher::LibrarySelect;
+using alcp::testing::utils::ArgsMap;
+using alcp::testing::utils::ParamType;
+using alcp::testing::utils::parseArgs;
 
 #if 1
 int
 main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
+    ArgsMap argsMap = parseArgs(argc, argv);
+    assert(argsMap["USE_OSSL"].paramType == ParamType::TYPE_BOOL);
+    assert(argsMap["USE_IPP"].paramType == ParamType::TYPE_BOOL);
+    assert(argsMap["OVERRIDE_ALCP"].paramType == ParamType::TYPE_BOOL);
+    // ::testing::RegisterTest("KnownAnswerTest",
+    // "GCM_Encrypt_experimental", )
+    if (std::get<bool>(argsMap["USE_OSSL"].value) == false
+        && std::get<bool>(argsMap["USE_IPP"].value) == false) {
+        RegisterMyTests<true>("KnownAnswerTest",
+                              "GCM_Encrypt_experimental_ALCP",
+                              LibrarySelect::ALCP);
+        RegisterMyTests<false>("KnownAnswerTest",
+                               "GCM_Decrypt_experimental_ALCP",
+                               LibrarySelect::ALCP);
+    }
+#ifdef USE_OSSL
+    if (std::get<bool>(argsMap["USE_OSSL"].value)) {
+        RegisterMyTests<true>("KnownAnswerTest",
+                              "GCM_Encrypt_experimental_OPENSSL",
+                              LibrarySelect::OPENSSL);
+        RegisterMyTests<false>("KnownAnswerTest",
+                               "GCM_Decrypt_experimental_OPENSSL",
+                               LibrarySelect::OPENSSL);
+    }
+#endif
+
+#ifdef USE_IPP
+    if (std::get<bool>(argsMap["USE_IPP"].value)) {
+        RegisterMyTests<true>("KnownAnswerTest",
+                              "GCM_Encrypt_experimental_IPP",
+                              LibrarySelect::IPP);
+        RegisterMyTests<false>("KnownAnswerTest",
+                               "GCM_Decrypt_experimental_IPP",
+                               LibrarySelect::IPP);
+    }
+#endif
+
     return RUN_ALL_TESTS();
 }
 #endif
