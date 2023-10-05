ade9f1a0e15143e80312875398a6d0a7ee015baf
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2023-10-05 08:55:56 +0000

    Crypto: Poly1305 Mac: Cross testing infra with OpenSSL
    -- Cross test function
    -- Cmake updates for openssl
    -- Other updates and fixes for Kat test function
    -- Updated csv file with a failing test vector
       (OpenSSL returns the correct mac, alcp doesnt)
    
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>

--- tests/include/poly1305/gtest_base_poly1305.hh
+++ tests/include/poly1305/gtest_base_poly1305.hh
@@ -37,6 +37,7 @@
 #include "gtest_common.hh"
 #include "poly1305/alc_poly1305.hh"
 #include "poly1305/poly1305.hh"
+#include "rng_base.hh"
 
 /* ALCP Headers */
 #include "alcp/alcp.h"
@@ -45,10 +46,12 @@
 #include "poly1305/openssl_poly1305.hh"
 #endif
 
-#define MAX_LOOP    1600
-#define KEY_LEN_MAX 1600
-#define INC_LOOP    1
-#define START_LOOP  1
+#define MAX_LOOP   16000
+#define INC_LOOP   16
+#define START_LOOP 16
+
+/* key len is fixed now */
+#define KEY_LEN 32
 
 /* print params verbosely */
 inline void
@@ -56,7 +59,8 @@ PrintmacTestData(std::vector<Uint8>   key,
                  alcp_poly1305_data_t data,
                  std::string          mode)
 {
-    std::cout << "KEY: " << parseBytesToHexStr(&key[0], key.size())
+    std::cout << "LIB: " << mode << std::endl;
+    std::cout << "KEY: " << parseBytesToHexStr(data.m_key, data.m_key_len)
               << " KeyLen: " << key.size() << std::endl;
     std::cout << "MSG: " << parseBytesToHexStr(data.m_msg, data.m_msg_len)
               << " MsgLen: " << data.m_msg_len << std::endl;
@@ -65,11 +69,105 @@ PrintmacTestData(std::vector<Uint8>   key,
     return;
 }
 
+/* cross test function */
+void
+Poly_Cross(alc_mac_info_t info)
+{
+    const int macSize      = 16;
+    info.mi_type           = ALC_MAC_POLY1305;
+    std::string LibStrMain = "ALCP", LibStrExt = "";
+
+    AlcpPoly1305Base apb(info);
+    RngBase          rb;
+    Poly1305Base *   pb_main, *pb_ext = nullptr;
+
+    pb_main = &apb;
+
+#ifdef USE_OSSL
+    OpenSSLPoly1305Base opb(info);
+    if ((useossl == true) || (pb_ext == nullptr)) {
+        pb_ext    = &opb;
+        LibStrExt = "OpenSSL";
+    }
+#endif
+
+    if (pb_ext == nullptr) {
+        std::cout << "No external lib selected!" << std::endl;
+        exit(-1);
+    }
+    /* generate message key data, use it chunk by chunk in the loop */
+    std::vector<Uint8> msg_full = rb.genRandomBytes(MAX_LOOP);
+    std::vector<Uint8> key_full = rb.genRandomBytes(KEY_LEN);
+
+    std::vector<Uint8>::const_iterator pos1, pos2;
+    auto                               rng = std::default_random_engine{};
+
+    for (int i = START_LOOP; i < MAX_LOOP; i += INC_LOOP) {
+        alcp_poly1305_data_t data_main = {}, data_ext = {};
+        std::vector<Uint8>   MacMainLib(macSize, 0);
+        std::vector<Uint8>   MacExtLib(macSize, 0);
+
+        /* generate msg data from msg_full */
+        msg_full = ShuffleVector(msg_full, rng);
+        pos1     = msg_full.end() - i;
+        pos2     = msg_full.end();
+        std::vector<Uint8> msg(pos1, pos2);
+
+        /* generate random key value*/
+        key_full = ShuffleVector(key_full, rng);
+
+        /* load test data */
+        data_main.m_msg     = &(msg[0]);
+        data_main.m_msg_len = msg.size();
+        data_main.m_mac     = &(MacMainLib[0]);
+        data_main.m_mac_len = MacMainLib.size();
+        data_main.m_key     = &(key_full[0]);
+        data_main.m_key_len = key_full.size();
+
+        /* load ext test data */
+        data_ext.m_msg     = &(msg[0]);
+        data_ext.m_msg_len = msg.size();
+        data_ext.m_mac     = &(MacExtLib[0]);
+        data_ext.m_mac_len = MacExtLib.size();
+        data_ext.m_key     = &(key_full[0]);
+        data_ext.m_key_len = key_full.size();
+
+        /* run test with main lib */
+        if (!pb_main->init(info, key_full)) {
+            printf("Error in mac init\n");
+            FAIL();
+        }
+        if (!pb_main->mac(data_main)) {
+            std::cout << "Error in mac function" << std::endl;
+            FAIL();
+        }
+        if (verbose > 1)
+            PrintmacTestData(key_full, data_main, LibStrMain);
+
+        /* run test with ext lib */
+        if (!pb_ext->init(info, key_full)) {
+            printf("Error in mac ext init function\n");
+            FAIL();
+        }
+        if (!pb_ext->mac(data_ext)) {
+            std::cout << "Error in mac (ext lib) function" << std::endl;
+            FAIL();
+        }
+        if (verbose > 1)
+            PrintmacTestData(key_full, data_ext, LibStrExt);
+
+        /* now check if the macs match */
+        EXPECT_TRUE(ArraysMatch(MacMainLib, MacExtLib, macSize));
+    }
+    return;
+}
+
 void
 Poly_Kat(alc_mac_info_t info)
 {
     alcp_poly1305_data_t data    = {};
     const int            macSize = 16;
+    std::string          LibStr  = "ALCP";
 
     info.mi_type = ALC_MAC_POLY1305;
 
@@ -87,8 +185,10 @@ Poly_Kat(alc_mac_info_t info)
 
 #ifdef USE_OSSL
     OpenSSLPoly1305Base ocb(info);
-    if (useossl == true)
-        cb = &ocb;
+    if (useossl == true) {
+        cb     = &ocb;
+        LibStr = "OpenSSL";
+    }
 #endif
 #ifdef USE_IPP
     IPPmacBase icb(info);
@@ -101,7 +201,6 @@ Poly_Kat(alc_mac_info_t info)
         std::vector<Uint8> mac(macSize, 0);
 
         auto msg = csv.getVect("MESSAGE");
-
         auto key = csv.getVect("KEY");
 
         data.m_msg = &(msg[0]);
@@ -127,6 +226,9 @@ Poly_Kat(alc_mac_info_t info)
             FAIL();
         }
 
+        if (verbose > 1)
+            PrintmacTestData(key, data, LibStr);
+
         /*conv mac output into a vector */
         /* we need only the no of bytes needed, from the output */
         std::vector<Uint8> mac_vector(
@@ -135,6 +237,6 @@ Poly_Kat(alc_mac_info_t info)
             mac_vector,         // Actual output
             csv.getVect("MAC"), // expected output, from the csv test data
             csv,
-            "POLY1305"));
+            LibStr));
     }
 }
