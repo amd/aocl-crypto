13385e9667f7483250c5285a68a253892931d4f0
Abhiram S <abhiram.s@amd.com>
2023-10-12 21:35:23 +0000

    Testing:Cipher:GCM:EXP: Cross Testing, use new mt19937
    Signed-off-by: Abhiram S <abhiram.s@amd.com>

--- tests/cipher_experimental/test_gcm_cross.cc
+++ tests/cipher_experimental/test_gcm_cross.cc
@@ -42,21 +42,6 @@ using alcp::testing::Csv;
 
 namespace alcp::testing::cipher::gcm {
 
-#if 0
-        // Will very from some x to y..
-        std::vector<Uint8> datasetPlainText  
-        // Intialization vector 96 bits to 128 bits
-        std::vector<Uint8> datasetInitvector
-        // 128 bites -> 256 bits
-        std::vector<Uint8> datasetKey        
-        // Same as PT
-        std::vector<Uint8> datasetCipherText 
-        // Can be less than or equal to 2**64 -1
-        std::vector<Uint8> datasetAddData   
-        // Tag size 128, 120, 112, 104, or 96 bits. 64 and 32 bits 
-        std::vector<Uint8> datasetTag
-#endif
-
 struct gcm_test_data
 {
     std::vector<Uint8> key;
@@ -67,38 +52,6 @@ struct gcm_test_data
     std::vector<Uint8> tag;
     Uint64             chunkSize;
 };
-
-// FIXME: Performance might be low.
-Uint64
-genRandom(std::vector<Uint8>& buffer)
-{
-
-    static RngBase     rngBase;
-    std::vector<Uint8> seed_v = rngBase.genRandomBytes(sizeof(Uint64));
-    Uint64             seed;
-
-    std::copy(&seed_v[0],
-              &seed_v[0] + seed_v.size(),
-              reinterpret_cast<Uint8*>(&seed));
-
-    std::mt19937 mt_rand(seed);
-    {
-        size_t iter = buffer.size() / 4;
-        for (size_t i = 0; i < iter; i++) {
-            Uint32 r   = mt_rand();
-            Uint8* r_8 = reinterpret_cast<Uint8*>(&r);
-            std::copy(r_8, r_8 + 4, (&buffer[0]) + (i * 4));
-        }
-    }
-    int rem = buffer.size() % 4;
-    if (rem) {
-        Uint32 r   = mt_rand();
-        Uint8* r_8 = reinterpret_cast<Uint8*>(&r);
-        std::copy(r_8, r_8 + rem, ((&buffer[0]) + (buffer.size() - 1) - rem));
-    }
-
-    return seed;
-}
 
 /**
  * @brief KAT Driver to test if library passes with given data
@@ -122,34 +75,34 @@ GcmCross_KAT(gcm_test_data& data, std::shared_ptr<ITestCipher> iTestCipher)
         std::max(datasetPlainText.size(), datasetCipherText.size()), 1);
     std::vector<Uint8> tagbuff(datasetTag.size());
 
-    alc_test_gcm_init_data_t dataInit;
-    dataInit.m_iv      = &datasetInitvector[0];
-    dataInit.m_iv_len  = datasetInitvector.size();
-    dataInit.m_aad     = &datasetAddData[0];
-    dataInit.m_aad_len = datasetAddData.size();
-    dataInit.m_key     = &datasetKey[0];
-    dataInit.m_key_len = datasetKey.size();
+    alc_test_gcm_init_data_t data_init;
+    data_init.m_iv      = &datasetInitvector[0];
+    data_init.m_iv_len  = datasetInitvector.size();
+    data_init.m_aad     = &datasetAddData[0];
+    data_init.m_aad_len = datasetAddData.size();
+    data_init.m_key     = &datasetKey[0];
+    data_init.m_key_len = datasetKey.size();
 
-    alc_test_gcm_update_data_t dataUpdate;
-    dataUpdate.m_iv         = &datasetInitvector[0];
-    dataUpdate.m_iv_len     = datasetInitvector.size();
-    dataUpdate.m_output     = &output[0];
-    dataUpdate.m_output_len = output.size();
+    alc_test_gcm_update_data_t data_update;
+    data_update.m_iv         = &datasetInitvector[0];
+    data_update.m_iv_len     = datasetInitvector.size();
+    data_update.m_output     = &output[0];
+    data_update.m_output_len = output.size();
 
     if constexpr (encryptor) { // Encrypt
-        dataUpdate.m_input = &datasetPlainText[0];
+        data_update.m_input = &datasetPlainText[0];
     } else { // Decrypt
-        dataUpdate.m_input = &datasetCipherText[0];
+        data_update.m_input = &datasetCipherText[0];
     }
-    dataUpdate.m_input_len = data.chunkSize;
+    data_update.m_input_len = data.chunkSize;
 
-    alc_test_gcm_finalize_data_t dataFinalize;
-    dataFinalize.m_tag_expected = &datasetTag[0];
-    dataFinalize.m_tag_len      = datasetTag.size();
-    dataFinalize.m_tag          = &tagbuff[0];
-    dataFinalize.m_out          = dataUpdate.m_output; // If needed for padding
-    dataFinalize.m_pt_len       = datasetPlainText.size();
-    dataFinalize.verified       = false;
+    alc_test_gcm_finalize_data_t data_finalize;
+    data_finalize.m_tag_expected = &datasetTag[0];
+    data_finalize.m_tag_len      = datasetTag.size();
+    data_finalize.m_tag          = &tagbuff[0];
+    data_finalize.m_out    = data_update.m_output; // If needed for padding
+    data_finalize.m_pt_len = datasetPlainText.size();
+    data_finalize.verified = false;
 
     Uint64 chunks, extra_bytes;
 
@@ -161,17 +114,17 @@ GcmCross_KAT(gcm_test_data& data, std::shared_ptr<ITestCipher> iTestCipher)
         extra_bytes = (data.cipherText.size() - (chunks * data.chunkSize));
     }
 
-    ASSERT_TRUE(iTestCipher->init(&dataInit));
+    ASSERT_TRUE(iTestCipher->init(&data_init));
     for (int i = 0; i < chunks; i++) {
-        ASSERT_TRUE(iTestCipher->update(&dataUpdate));
-        dataUpdate.m_input += data.chunkSize;
-        dataUpdate.m_output += data.chunkSize;
+        ASSERT_TRUE(iTestCipher->update(&data_update));
+        data_update.m_input += data.chunkSize;
+        data_update.m_output += data.chunkSize;
     }
     if (extra_bytes) {
-        dataUpdate.m_input_len = extra_bytes;
-        ASSERT_TRUE(iTestCipher->update(&dataUpdate));
+        data_update.m_input_len = extra_bytes;
+        ASSERT_TRUE(iTestCipher->update(&data_update));
     }
-    ASSERT_TRUE(iTestCipher->finalize(&dataFinalize));
+    ASSERT_TRUE(iTestCipher->finalize(&data_finalize));
 
     if constexpr (encryptor) { // Encrypt
         ASSERT_EQ(output, datasetCipherText);
@@ -191,95 +144,97 @@ GcmCross_KAT(gcm_test_data& data, std::shared_ptr<ITestCipher> iTestCipher)
 void
 GcmCross_REF(gcm_test_data& data, std::shared_ptr<ITestCipher> iTestCipher)
 {
-    std::vector<Uint8> datasetPlainText  = data.plainText;
-    std::vector<Uint8> datasetInitvector = data.initVector;
-    std::vector<Uint8> datasetKey        = data.key;
-    std::vector<Uint8> datasetAddData    = data.additionalText;
+    std::vector<Uint8> dataset_plain_text = data.plainText;
+    std::vector<Uint8> dataset_initvector = data.initVector;
+    std::vector<Uint8> dataset_key        = data.key;
+    std::vector<Uint8> dataset_add_data   = data.additionalText;
 
     std::vector<Uint8> tagbuff(data.tag.size());
 
-    alc_test_gcm_init_data_t dataInit;
-    dataInit.m_iv      = &datasetInitvector[0];
-    dataInit.m_iv_len  = datasetInitvector.size();
-    dataInit.m_aad     = &datasetAddData[0];
-    dataInit.m_aad_len = datasetAddData.size();
-    dataInit.m_key     = &datasetKey[0];
-    dataInit.m_key_len = datasetKey.size();
+    alc_test_gcm_init_data_t data_init;
+    data_init.m_iv      = &dataset_initvector[0];
+    data_init.m_iv_len  = dataset_initvector.size();
+    data_init.m_aad     = &dataset_add_data[0];
+    data_init.m_aad_len = dataset_add_data.size();
+    data_init.m_key     = &dataset_key[0];
+    data_init.m_key_len = dataset_key.size();
 
-    alc_test_gcm_update_data_t dataUpdate;
-    dataUpdate.m_iv         = &datasetInitvector[0];
-    dataUpdate.m_iv_len     = datasetInitvector.size();
-    dataUpdate.m_output     = &data.cipherText[0];
-    dataUpdate.m_output_len = data.cipherText.size();
-    dataUpdate.m_input      = &datasetPlainText[0];
-    dataUpdate.m_input_len  = data.chunkSize;
+    alc_test_gcm_update_data_t data_update;
+    data_update.m_iv         = &dataset_initvector[0];
+    data_update.m_iv_len     = dataset_initvector.size();
+    data_update.m_output     = &data.cipherText[0];
+    data_update.m_output_len = data.cipherText.size();
+    data_update.m_input      = &dataset_plain_text[0];
+    data_update.m_input_len  = data.chunkSize;
 
-    alc_test_gcm_finalize_data_t dataFinalize;
-    dataFinalize.m_tag_expected = &data.tag[0];
-    dataFinalize.m_tag_len      = data.tag.size();
-    dataFinalize.m_tag          = &data.tag[0];
-    dataFinalize.m_out          = dataUpdate.m_output; // If needed for padding
-    dataFinalize.m_pt_len       = datasetPlainText.size();
-    dataFinalize.verified       = false;
+    alc_test_gcm_finalize_data_t data_finalize;
+    data_finalize.m_tag_expected = &data.tag[0];
+    data_finalize.m_tag_len      = data.tag.size();
+    data_finalize.m_tag          = &data.tag[0];
+    data_finalize.m_out    = data_update.m_output; // If needed for padding
+    data_finalize.m_pt_len = dataset_plain_text.size();
+    data_finalize.verified = false;
 
     Uint64 chunks      = data.plainText.size() / data.chunkSize;
     Uint64 extra_bytes = (data.plainText.size() - (chunks * data.chunkSize));
 
-    ASSERT_TRUE(iTestCipher->init(&dataInit));
+    ASSERT_TRUE(iTestCipher->init(&data_init));
     for (int i = 0; i < chunks; i++) {
-        ASSERT_TRUE(iTestCipher->update(&dataUpdate));
-        dataUpdate.m_input += data.chunkSize;
-        dataUpdate.m_output += data.chunkSize;
+        ASSERT_TRUE(iTestCipher->update(&data_update));
+        data_update.m_input += data.chunkSize;
+        data_update.m_output += data.chunkSize;
     }
     if (extra_bytes) {
-        dataUpdate.m_input_len = extra_bytes;
-        ASSERT_TRUE(iTestCipher->update(&dataUpdate));
+        data_update.m_input_len = extra_bytes;
+        ASSERT_TRUE(iTestCipher->update(&data_update));
     }
-    ASSERT_TRUE(iTestCipher->finalize(&dataFinalize));
+    ASSERT_TRUE(iTestCipher->finalize(&data_finalize));
 }
 
 void
-CrossTestGCM(LibrarySelect select1, LibrarySelect select2)
+CrossTestGCM(std::shared_ptr<RngBase> rng,
+             LibrarySelect            select1,
+             LibrarySelect            select2)
 {
-    const std::vector<Uint64> initVectSizes = {
+    const std::vector<Uint64> cInitVectSizes = {
         128, 120, 112, 104, 96
     }; // bits
-    const std::vector<Uint64> additionalText_sizes = {
+    const std::vector<Uint64> cAdditionalTextSizes = {
         20000, 128, 120, 112, 104, 96, 64, 32
     };                                                       // bits
-    const std::vector<Uint64> key_sizes = { 128, 192, 256 }; // bits
-    const std::vector<Uint64> tagSizes  = {
+    const std::vector<Uint64> cKeySizes = { 128, 192, 256 }; // bits
+    const std::vector<Uint64> cTagSizes = {
         128, 120, 112, 104, 96, 64, 32
-    };                                                         // bits
-    const std::vector<Uint64> chunkSizes = { 16, 32 };         // bits
-    const std::vector<Uint64> ptSizes    = { 16, 1000, 2563 }; // bytes
+    };                                                          // bits
+    const std::vector<Uint64> cChunkSizes = { 16, 32 };         // bits
+    const std::vector<Uint64> cPtSizes    = { 16, 1000, 2563 }; // bytes
 
     gcm_test_data test_data;
 
-    Uint64 testCount = 0;
+    Uint64 test_count = 0;
 
     // Constant Plaintext Test
-    for (Uint64 key_size : key_sizes) {
-        for (Uint64 additionalText_size : additionalText_sizes) {
-            for (Uint64 tagSize : tagSizes) {
-                for (int initVectSize : initVectSizes) {
-                    for (int chunkSize : chunkSizes) {
-                        for (int ptSize : ptSizes) {
+    for (Uint64 key_size : cKeySizes) {
+        for (Uint64 additional_text_size : cAdditionalTextSizes) {
+            for (Uint64 tag_size : cTagSizes) {
+                for (int init_vect_size : cInitVectSizes) {
+                    for (int chunk_size : cChunkSizes) {
+                        for (int pt_size : cPtSizes) {
                             test_data.key = std::vector<Uint8>(key_size / 8);
                             test_data.additionalText =
-                                std::vector<Uint8>(additionalText_size / 8);
+                                std::vector<Uint8>(additional_text_size / 8);
                             test_data.initVector =
-                                std::vector<Uint8>(initVectSize / 8);
-                            test_data.plainText = std::vector<Uint8>(ptSize);
+                                std::vector<Uint8>(init_vect_size / 8);
+                            test_data.plainText = std::vector<Uint8>(pt_size);
                             test_data.cipherText =
                                 std::vector<Uint8>(test_data.plainText.size());
-                            test_data.tag = std::vector<Uint8>(tagSize / 8);
-                            test_data.chunkSize = chunkSize;
+                            test_data.tag = std::vector<Uint8>(tag_size / 8);
+                            test_data.chunkSize = chunk_size;
 
-                            genRandom(test_data.key);
-                            genRandom(test_data.additionalText);
-                            genRandom(test_data.initVector);
-                            genRandom(test_data.plainText);
+                            rng->genRandomMt19937(test_data.key);
+                            rng->genRandomMt19937(test_data.additionalText);
+                            rng->genRandomMt19937(test_data.initVector);
+                            rng->genRandomMt19937(test_data.plainText);
 
                             GcmCross_REF(test_data,
                                          GcmCipherFactory<true>(select1));
@@ -290,19 +245,60 @@ CrossTestGCM(LibrarySelect select1, LibrarySelect select2)
                             GcmCross_KAT<false>(
                                 test_data, GcmCipherFactory<false>(select2));
 
-                            testCount++;
+                            test_count++;
                         }
                     }
                 }
             }
         }
     }
-    std::cout << "Tests Executed:" << testCount << std::endl;
+
+    const Uint64 pt_min_size = 16;
+    const Uint64 pt_max_size = 160000;
+    const Uint64 pt_dec_size = 31;
+
+    const Uint64 cKeySize            = cKeySizes.back();
+    const Uint64 cAdditionalTextSize = cAdditionalTextSizes.back();
+    const Uint64 cTagSize            = cTagSizes.back();
+    const Uint64 cInitVectSize       = cInitVectSizes.back();
+    const Uint64 cChunkSize          = cChunkSizes.back();
+
+    test_data.key            = std::vector<Uint8>(cKeySize / 8);
+    test_data.additionalText = std::vector<Uint8>(cAdditionalTextSize / 8);
+    test_data.initVector     = std::vector<Uint8>(cInitVectSize / 8);
+    test_data.tag            = std::vector<Uint8>(cTagSize / 8);
+    test_data.plainText      = std::vector<Uint8>(pt_max_size);
+    test_data.chunkSize      = cChunkSize;
+    for (int pt_size = pt_max_size; pt_size >= pt_min_size;
+         pt_size -= pt_dec_size) {
+
+        // Change size without reallocation
+        test_data.plainText.resize(pt_size);
+        test_data.plainText.shrink_to_fit();
+        test_data.cipherText.resize(pt_size);
+        test_data.cipherText.shrink_to_fit();
+
+        rng->genRandomMt19937(test_data.key);
+        rng->genRandomMt19937(test_data.additionalText);
+        rng->genRandomMt19937(test_data.initVector);
+        rng->genRandomMt19937(test_data.plainText);
+
+        GcmCross_REF(test_data, GcmCipherFactory<true>(select1));
+
+        GcmCross_KAT<true>(test_data, GcmCipherFactory<true>(select2));
+
+        GcmCross_KAT<false>(test_data, GcmCipherFactory<false>(select2));
+
+        test_count++;
+    }
+    std::cout << "Tests Executed:" << test_count << std::endl;
 }
 
 // Reference :
 // https://google.github.io/googletest/advanced.html#registering-tests-programmatically
-class MyFixture : public ::testing::Test
+
+// FIXME: We can move glow code to common code
+class CrossTestFixture : public ::testing::Test
 {
   public:
     // All of these optional, just like in regular macro usage.
@@ -312,25 +308,31 @@ class MyFixture : public ::testing::Test
     void        TearDown() override {}
 };
 
-class MyTest : public MyFixture
+class CrossTest : public CrossTestFixture
 {
   public:
-    explicit MyTest(LibrarySelect select1, LibrarySelect select2)
+    explicit CrossTest(std::shared_ptr<RngBase> rng,
+                       LibrarySelect            select1,
+                       LibrarySelect            select2)
         : _select1(select1)
         , _select2(select2)
-    {}
-    void TestBody() override { CrossTestGCM(_select1, _select2); }
+    {
+        _rng = rng;
+    }
+    void TestBody() override { CrossTestGCM(_rng, _select1, _select2); }
 
   private:
     LibrarySelect                _select1, _select2;
+    std::shared_ptr<RngBase>     _rng;
     std::unique_ptr<ITestCipher> iTestCipher1, iTestCipher2;
 };
 
 void
-RegisterMyTests(std::string         testSuiteName,
-                std::string         testCaseName,
-                const LibrarySelect select1,
-                const LibrarySelect select2)
+RegisterMyTests(std::string              testSuiteName,
+                std::string              testCaseName,
+                std::shared_ptr<RngBase> rng,
+                const LibrarySelect      select1,
+                const LibrarySelect      select2)
 {
     ::testing::RegisterTest(
         testSuiteName.c_str(),
@@ -340,7 +342,9 @@ RegisterMyTests(std::string         testSuiteName,
         __FILE__,
         __LINE__,
         // Important to use the fixture type as the return type here.
-        [=]() -> MyFixture* { return new MyTest(select1, select2); });
+        [=]() -> CrossTestFixture* {
+            return new CrossTest(rng, select1, select2);
+        });
 }
 } // namespace alcp::testing::cipher::gcm
 
@@ -355,6 +359,10 @@ int
 main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
+
+    std::shared_ptr<alcp::testing::RngBase> rng =
+        std::make_shared<alcp::testing::RngBase>();
+
     ArgsMap argsMap = parseArgs(argc, argv);
     assert(argsMap["USE_OSSL"].paramType == ParamType::TYPE_BOOL);
     assert(argsMap["USE_IPP"].paramType == ParamType::TYPE_BOOL);
@@ -366,6 +374,7 @@ main(int argc, char** argv)
     if (std::get<bool>(argsMap["USE_OSSL"].value)) {
         RegisterMyTests("KnownAnswerTest",
                         "GCM_CROSS_EXPERIMENTAL_OPENSSL",
+                        rng,
                         LibrarySelect::OPENSSL,
                         LibrarySelect::ALCP);
     }
@@ -375,6 +384,7 @@ main(int argc, char** argv)
     if (std::get<bool>(argsMap["USE_IPP"].value)) {
         RegisterMyTests("KnownAnswerTest",
                         "GCM_CROSS_EXPERIMENTAL_IPP",
+                        rng,
                         LibrarySelect::IPP,
                         LibrarySelect::ALCP);
     }
