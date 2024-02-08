209c7a3471cd8e7c035f882b0999d42006f8a052
Abhijith N Raj <abhijith.raj@amd.com>
2024-02-08 07:04:01 +0000

    Compat:OpenSSL:Digest: Use OPENSSL_clear_free in ALCP_prov_digest_freectx
    OpenSSL_free causes double free issues if free is called again.
    OpenSSL_clear_free does not

--- lib/compat/openssl/digest/alcp_digest_prov.c
+++ lib/compat/openssl/digest/alcp_digest_prov.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023, Advanced Micro Devices. All rights reserved.
+ * Copyright (C) 2023-2024, Advanced Micro Devices. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -40,7 +40,7 @@ ALCP_prov_digest_freectx(void* vctx)
 
     EVP_MD_CTX_free(pdctx->pc_evp_digest_ctx);
 
-    OPENSSL_free(vctx);
+    OPENSSL_clear_free(vctx, sizeof(*pdctx));
 }
 
 void*
