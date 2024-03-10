79f7478a10f71c60c954529c121ed3c0aec5651f
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2024-03-10 10:26:39 +0000

    Crypto: Cmake: Cmake function to pass custom args such as valgrind runs, pass lib options like ipp,openssl,etc
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>

---
+++ cmake/AlcpTestUtils.cmake
@@ -0,0 +1,47 @@
+# Copyright (C) 2024, Advanced Micro Devices. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+include(CMakeParseArguments)
+include(GoogleTest)
+
+function(alcp_add_valgrind_check_test test_name test_binary)
+    find_program(VALGRIND "valgrind")
+    # add more valgrind options here
+    set(VALGRIND_ARGS "--leak-check=full")
+    add_test(${test_name}_valgrind ${VALGRIND} ${VALGRIND_ARGS} ${test_binary})
+endfunction(alcp_add_valgrind_check_test)
+
+function(alcp_add_integration_tests test_name test_binary)
+    set(IPP_ARGS "-i")
+    set(OPENSSL_ARGS "-o")
+    #set(VERBOSE_ARGS "-v 2")
+    if(ENABLE_TESTS_IPP_API)
+        add_test(${test_name}_ipp ${test_binary} ${IPP_ARGS})
+    endif(ENABLE_TESTS_IPP_API)
+    if (ENABLE_TESTS_OPENSSL_API)
+        add_test(${test_name}_openssl ${test_binary} ${OPENSSL_ARGS})
+    endif(ENABLE_TESTS_OPENSSL_API)
+endfunction(alcp_add_integration_tests)
