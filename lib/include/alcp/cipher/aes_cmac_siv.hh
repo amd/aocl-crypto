255b3333a0c34b475aa224f375eb51c82236d540
Abhiram S <abhiram.s@amd.com>
2024-04-08 10:36:22 +0000

    lib:Cipher:AES:SIV: Move out of Impl

--- lib/include/alcp/cipher/aes_cmac_siv.hh
+++ lib/include/alcp/cipher/aes_cmac_siv.hh
@@ -57,6 +57,30 @@ class ALCP_API_EXPORT CmacSiv : public Aes
     class Impl;
     std::unique_ptr<Impl> pImpl;
 
+    // FIXME: Impl to class migration
+  private:
+    /*
+       Set of preprocessed Additional Data. Its allocated in a chunk to avoid
+       memory issues. Each chunk being 10 slots. Any number of addtional data
+       can be given by user but most of the time it will be less than 10. So a
+       default size of 10 is allocated.
+    */
+    // std::vector<std::vector<Uint8>> m_additionalDataProcessed =
+    //     std::vector<std::vector<Uint8>>(10);
+    std::vector<std::vector<Uint8>> m_additionalDataProcessed =
+        std::vector<std::vector<Uint8>>(10);
+    Uint64       m_additionalDataProcessedSize = {};
+    const Uint8* m_key1                        = {};
+    const Uint8* m_key2                        = {};
+    Uint64       m_keyLength                   = {};
+    Uint64       m_padLen                      = {};
+    const Uint8* m_iv                          = {};
+    alignas(16) Uint8 m_cmacTemp[SIZE_CMAC]    = {};
+    Cmac              m_cmac;
+    alc_cipher_data_t data;
+    T1* m_ctr; // FIXME: based on the key size appropriate Ctr class
+               // to be choosen.
+
     // FIXME: Need to be private or need some friend function thing
   protected:
     /**
@@ -70,6 +94,8 @@ class ALCP_API_EXPORT CmacSiv : public Aes
   public:
     CmacSiv();
     CmacSiv(alc_cipher_data_t* ctx);
+    CmacSiv(const alc_key_info_t& encKey, const alc_key_info_t& authKey);
+    ~CmacSiv();
     /**
      * @brief Set Keys for SIV and CTR
      * @param key1  Key for SIV
@@ -127,8 +153,6 @@ class ALCP_API_EXPORT CmacSiv : public Aes
                               Uint8*       pPlainText,
                               Uint64       len);
 
-    CmacSiv(const alc_key_info_t& encKey, const alc_key_info_t& authKey);
-
     /* Depriciated Functions */
     // FIXME: Needs to be removed from Cipher as a whole
 
@@ -170,6 +194,41 @@ class ALCP_API_EXPORT CmacSiv : public Aes
                      Uint64             keyLen,
                      const Uint8*       pIv,
                      Uint64             ivLen);
+
+  private:
+    /**
+     * @brief Do Cmac implementation
+     * @param data Pointer to data to do cmac on
+     * @param size Size of the data
+     * @param mac OutputMac memory
+     * @param macSize Size of Mac
+     * @param enc Encrypt (True) or Decrypt (False)
+     * @return Status
+     */
+    Status ctrWrapper(
+        const Uint8 in[], Uint8 out[], Uint64 size, Uint8 mac[], bool enc);
+
+    Status cmacWrapper(const Uint8 data[],
+                       Uint64      size,
+                       Uint8       mac[],
+                       Uint64      macSize);
+
+    /**
+     * @brief Do Cmac implementation
+     * @param data1 Pointer to data1 to do cmac on
+     * @param size1 Size of the data1
+     * @param data2 Pointer to data2 to do cmac on
+     * @param size2 Size of the data2
+     * @param mac OutputMac memory
+     * @param macSize Size of Mac
+     * @return Status
+     */
+    Status cmacWrapperMultiData(const Uint8 data1[],
+                                Uint64      size1,
+                                const Uint8 data2[],
+                                Uint64      size2,
+                                Uint8       mac[],
+                                Uint64      macSize);
 };
 
 template<typename T>
@@ -678,6 +737,7 @@ CmacSiv<T>::Impl::init(const Uint8* pKey,
 template<typename T>
 CmacSiv<T>::CmacSiv()
     : pImpl{ std::make_unique<Impl>() }
+    , m_ctr{ new T(&data) }
 {
 }
 
@@ -685,6 +745,7 @@ template<typename T>
 CmacSiv<T>::CmacSiv(alc_cipher_data_t* ctx)
     : Aes(ctx)
     , pImpl{ std::make_unique<Impl>() }
+    , m_ctr{ new T(&data) }
 {
 }
 
@@ -694,6 +755,12 @@ CmacSiv<T>::CmacSiv(const alc_key_info_t& encKey, const alc_key_info_t& authKey)
 {
     assert(authKey.len == encKey.len);
     setKeys(authKey.key, encKey.key, encKey.len);
+}
+
+template<typename T>
+CmacSiv<T>::~CmacSiv()
+{
+    delete m_ctr;
 }
 
 template<typename T>
@@ -715,21 +782,126 @@ CmacSiv<T>::init(alc_cipher_data_t* ctx,
                  const Uint8*       pIv,
                  Uint64             ivLen)
 {
+#if USE_IMPL == 0
+    m_keyLength = keyLen / 8;
+    m_key1      = pKey;
+    m_key2      = pKey + m_keyLength;
+    m_iv        = pIv;
+    setKeys(m_key1, m_key2, m_keyLength);
+    return ALC_ERROR_NONE;
+#else
     return pImpl->init(pKey, keyLen, pIv, ivLen);
+#endif
 }
 
 template<typename T>
 Status
 CmacSiv<T>::s2v(const Uint8 plainText[], Uint64 size)
 {
+#if USE_IMPL == 0
+    // Assume plaintest to be 128 bit multiples.
+    Status             s    = StatusOk();
+    std::vector<Uint8> zero = std::vector<Uint8>(SIZE_CMAC, 0);
+
+    // Do a cmac of Zero Vector, first additonal data.
+    s = cmacWrapper(&(zero.at(0)), zero.size(), m_cmacTemp, SIZE_CMAC);
+
+    if (!s.ok()) {
+        return s;
+    }
+
+    // std::cout << "ZERO_VECT:" << parseBytesToHexStr(m_cmacTemp) << std::endl;
+
+    Uint8 rb[16] = {};
+    rb[15]       = 0x87;
+
+    // For each user provided additional data do the dbl and xor to complete
+    // processing
+    if (CpuId::cpuHasAvx2()) {
+        avx2::processAad(m_cmacTemp,
+                         m_additionalDataProcessed,
+                         m_additionalDataProcessedSize);
+    } else {
+        for (Uint64 i = 0; i < m_additionalDataProcessedSize; i++) {
+
+            alcp::cipher::dbl(&(m_cmacTemp[0]), rb);
+
+            // std::cout << "dbl:" << parseBytesToHexStr(m_cmacTemp) <<
+            // std::endl;
+
+            alcp::cipher::xor_a_b(&m_cmacTemp[0],
+                                  &(m_additionalDataProcessed.at(i).at(0)),
+                                  &m_cmacTemp[0],
+                                  SIZE_CMAC);
+        }
+    }
+
+    // If the size of plaintext is lower there is special case
+    if (size >= SIZE_CMAC) {
+
+        // Take out last block
+        if (CpuId::cpuIsZen3()) {
+            zen3::xor_a_b((plainText + size - SIZE_CMAC),
+                          m_cmacTemp,
+                          m_cmacTemp,
+                          SIZE_CMAC);
+        } else {
+            xor_a_b((plainText + size - SIZE_CMAC),
+                    m_cmacTemp,
+                    m_cmacTemp,
+                    SIZE_CMAC);
+        }
+
+        s = cmacWrapperMultiData(plainText,
+                                 (size - SIZE_CMAC),
+                                 m_cmacTemp,
+                                 SIZE_CMAC,
+                                 m_cmacTemp,
+                                 SIZE_CMAC);
+    } else {
+        Uint8 temp_bytes[16] = {};
+        // Padding Hack
+        temp_bytes[0] = 0x80;
+        // Speical case size lower for plain text need to do double and padding
+        if (CpuId::cpuHasAvx2()) {
+            avx2::dbl(&(m_cmacTemp[0]));
+        }
+        // alcp::cipher::dbl(&(m_cmacTemp[0]), rb, &(m_cmacTemp[0]));
+        // std::cout << "dbl:" << parseBytesToHexStr(m_cmacTemp) << std::endl;
+
+        xor_a_b(plainText, m_cmacTemp, m_cmacTemp, size);
+        // Padding
+        xor_a_b(
+            temp_bytes, m_cmacTemp + size, m_cmacTemp + size, (SIZE_CMAC)-size);
+
+        // std::cout << "xor:" << parseBytesToHexStr(m_cmacTemp) << std::endl;
+
+        s = cmacWrapper(m_cmacTemp, SIZE_CMAC, m_cmacTemp, SIZE_CMAC);
+    }
+    if (!s.ok()) {
+        return s;
+    }
+    // std::cout << "V:  " << parseBytesToHexStr(m_cmacTemp) << std::endl;
+    // Now m_cmacTemp is the offical SIV
+    return s;
+#else
     return pImpl->s2v(plainText, size);
+#endif
 }
 
 template<typename T>
 Status
 CmacSiv<T>::getTag(Uint8 out[])
 {
+#if USE_IMPL == 0
+    Status s = StatusOk();
+    utils::CopyBytes(out, &m_cmacTemp[0], SIZE_CMAC);
+    memset(&m_cmacTemp[0], 0, 16);
+    m_additionalDataProcessedSize = 0;
+    return s;
+#else
     return pImpl->getTag(out);
+#endif
 }
 
 template<typename T>
@@ -751,14 +923,46 @@ template<typename T>
 Status
 CmacSiv<T>::setKeys(const Uint8 key1[], const Uint8 key2[], Uint64 length)
 {
+#if USE_IMPL == 0
+    Status s    = StatusOk();
+    m_keyLength = length;
+
+    // Block all unknown keysizes
+    switch (length) {
+        case 16:
+        case 24:
+        case 32:
+            break;
+        default:
+            auto cer = cipher::CipherError(cipher::ErrorCode::eInvaidValue);
+            s.update(cer, cer.message());
+            return s;
+    }
+
+    m_key1 = key1;
+    m_key2 = key2;
+
+    s = m_cmac.setKey(m_key1, m_keyLength * 8);
+    if (!s.ok()) {
+        return s;
+    }
+
+    // m_ctr->setKey(m_key2, m_keyLength);
+    return s;
+#else
     return pImpl->setKeys(key1, key2, length);
+#endif
 }
 
 template<typename T>
 alc_error_t
 CmacSiv<T>::setAad(alc_cipher_data_t* ctx, const Uint8 memory[], Uint64 length)
 {
+#if USE_IMPL == 0
+    Status s = addAdditionalInput(memory, length);
+#else
     Status s = pImpl->addAdditionalInput(memory, length);
+#endif
     if (s.ok()) {
         return ALC_ERROR_NONE;
     } else {
@@ -770,14 +974,59 @@ template<typename T>
 Status
 CmacSiv<T>::addAdditionalInput(const Uint8 memory[], Uint64 length)
 {
+#if USE_IMPL == 0
+    Status s = StatusOk();
+
+    // FIXME: Allocate SIZE_CMAC for 10 vectors on intialization to be more
+    // optimal.
+
+    // Extend size of additonalDataProcessed Vector in case of overflow
+    if ((m_additionalDataProcessedSize + 1)
+        == m_additionalDataProcessed.size()) {
+        m_additionalDataProcessed.resize(m_additionalDataProcessed.size() + 10);
+    }
+
+    // Block Null Keys or non set Keys.
+    if (m_key1 == nullptr || m_key2 == nullptr) {
+        auto cer = cipher::CipherError(cipher::ErrorCode::eInvaidValue);
+        s.update(cer, cer.message());
+        return s;
+    }
+
+    // Allocate memory for additonal data processed vector
+    m_additionalDataProcessed.at(m_additionalDataProcessedSize) =
+        std::vector<Uint8>(SIZE_CMAC);
+
+    // Do cmac for additional data and set it to the proceed data.
+    s = cmacWrapper(
+        memory,
+        length,
+        &((m_additionalDataProcessed.at(m_additionalDataProcessedSize)).at(0)),
+        SIZE_CMAC);
+
+    if (!s.ok()) {
+        return s;
+    }
+
+    // Increment the size of Data Processed if no errors
+    m_additionalDataProcessedSize += 1;
+    return s;
+#else
     return pImpl->addAdditionalInput(memory, length);
+#endif
 }
 
 template<typename T>
 Status
 CmacSiv<T>::setPaddingLen(Uint64 len)
 {
+#if USE_IMPL == 0
+    Status s = StatusOk();
+    m_padLen = len;
+    return s;
+#else
     return pImpl->setPaddingLen(len);
+#endif
 }
 
 template<typename T>
@@ -787,6 +1036,37 @@ CmacSiv<T>::encryptUpdate(void*        ctx,
                           Uint8*       pCipherText,
                           Uint64       len)
 {
+#if USE_IMPL == 0
+    alc_error_t err = ALC_ERROR_NONE;
+    Status      s   = StatusOk();
+
+    // Mask Vector for disabling 2 bits in the counter
+    Uint8 q[16] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+                    0x7f, 0xff, 0xff, 0xff, 0x7f, 0xff, 0xff, 0xff };
+
+    s = s2v(pPlainText, len);
+
+    if (!s.ok()) {
+        err = ALC_ERROR_GENERIC;
+        return err;
+    }
+
+    // Apply the mask and make q the IV
+    for (Uint64 i = 0; i < SIZE_CMAC; i++) {
+        q[i] = m_cmacTemp[i] & q[i];
+    }
+
+    data.m_keyLen_in_bytes = m_keyLength;
+
+    // Do the CTR
+    s = ctrWrapper(pPlainText, pCipherText, len + m_padLen, q, true);
+
+    if (!s.ok()) {
+        err = ALC_ERROR_GENERIC;
+        return err;
+    }
+    return err;
+#else
     alc_error_t err = ALC_ERROR_NONE;
 
     Status s = pImpl->encryptUpdate(pPlainText, pCipherText, len);
@@ -795,6 +1075,7 @@ CmacSiv<T>::encryptUpdate(void*        ctx,
     }
 
     return err;
+#endif
 }
 
 template<typename T>
@@ -805,6 +1086,51 @@ CmacSiv<T>::decryptUpdate(void*        ctx,
                           Uint64       len)
 
 {
+#if USE_IMPL == 0
+    Status      s   = StatusOk();
+    alc_error_t err = ALC_ERROR_NONE;
+
+    // Mask Vector for disabling 2 bits in the counter
+    Uint8 q[16] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+                    0x7f, 0xff, 0xff, 0xff, 0x7f, 0xff, 0xff, 0xff };
+
+    // Apply the mask and make q the IV
+    for (Uint64 i = 0; i < SIZE_CMAC; i++) {
+        q[i] = m_iv[i] & q[i];
+    }
+
+    // Do the CTR
+    s = ctrWrapper(pCipherText, pPlainText, len + m_padLen, q, false);
+
+    if (!s.ok()) {
+        err = ALC_ERROR_GENERIC;
+        return err;
+    }
+
+    // Create the tag from generated plain text
+    s = s2v(pPlainText, len);
+
+    // Verify tag, which just got generated
+    if (memcmp(&(m_cmacTemp[0]), m_iv, SIZE_CMAC) != 0) {
+// FIXME: Initiate Wipedown!
+#if 0
+        auto cer =
+            cipher::CipherError(cipher::ErrorCode::eAuthenticationFailure);
+        s.update(cer, cer.message());
+#else
+        err = ALC_ERROR_TAG_MISMATCH;
+        return err;
+#endif
+        if (!s.ok()) {
+            err = ALC_ERROR_GENERIC;
+            return err;
+        }
+    }
+    if (!s.ok()) {
+        err = ALC_ERROR_GENERIC;
+    }
+    return err;
+#else
     alc_error_t err = ALC_ERROR_NONE;
     Status      s   = pImpl->decryptUpdate(pCipherText, pPlainText, len);
     if (!s.ok()) {
@@ -812,6 +1138,64 @@ CmacSiv<T>::decryptUpdate(void*        ctx,
         // std::cout << "IV Verify Failed!" << std::endl;
     }
     return err;
+#endif
+}
+
+template<typename T>
+Status
+CmacSiv<T>::ctrWrapper(
+    const Uint8 in[], Uint8 out[], Uint64 size, Uint8 mac[], bool enc)
+{
+    Status s = StatusOk();
+
+    // FIXME: To be removed once we move everything to Status
+    alc_error_t err        = ALC_ERROR_NONE;
+    data.m_keyLen_in_bytes = m_keyLength;
+
+    // Initialize CTR with mac as IV
+    m_ctr->init(&data, m_key2, m_keyLength * 8, mac, 16);
+
+    if (enc) {
+        // FIXME: mac should go in seperate ctr init call.
+        err = m_ctr->encrypt(&data, in, out, size); //, mac);
+        if (alcp_is_error(err)) {
+            auto cer = status::EncryptFailed("Encryption Kernel Failed!");
+            s.update(cer);
+            return s;
+        }
+    } else {
+        // FIXME: mac should go in seperate ctr init call.
+        err = m_ctr->decrypt(&data, in, out, size); //, mac);
+        if (alcp_is_error(err)) {
+            auto cer = status::DecryptFailed("Decryption Kernel Failed!");
+            s.update(cer);
+            return s;
+        }
+    }
+    return s;
+}
+
+template<typename T>
+Status
+CmacSiv<T>::cmacWrapper(const Uint8 data[],
+                        Uint64      size,
+                        Uint8       mac[],
+                        Uint64      macSize)
+{
+    Status s{ StatusOk() };
+    s = m_cmac.finalize(data, size);
+    if (!s.ok()) {
+        return s;
+    }
+    s = m_cmac.copy(mac, macSize);
+    if (!s.ok()) {
+        return s;
+    }
+    s = m_cmac.reset();
+    if (!s.ok()) {
+        return s;
+    }
+    return s;
 }
 
 template<typename T>
@@ -828,6 +1212,35 @@ CmacSiv<T>::isSupported(const Uint32 keyLen)
 {
     // FIXME: Tobe Implemented
     return true;
+}
+
+template<typename T>
+Status
+CmacSiv<T>::cmacWrapperMultiData(const Uint8 data1[],
+                                 Uint64      size1,
+                                 const Uint8 data2[],
+                                 Uint64      size2,
+                                 Uint8       mac[],
+                                 Uint64      macSize)
+{
+    Status s{ StatusOk() };
+    s = m_cmac.update(data1, size1);
+    if (!s.ok()) {
+        return s;
+    }
+    s = m_cmac.finalize(data2, size2);
+    if (!s.ok()) {
+        return s;
+    }
+    s = m_cmac.copy(mac, macSize);
+    if (!s.ok()) {
+        return s;
+    }
+    s = m_cmac.reset();
+    if (!s.ok()) {
+        return s;
+    }
+    return s;
 }
 
 template class CmacSiv<aesni::Ctr128>;
