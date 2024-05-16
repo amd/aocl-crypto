01d9b0a6c24e09b6843d6c6d6aa9154f508861fb
Abhiram S <abhiram.s@amd.com>
2024-05-16 08:35:01 +0000

    Examples:Cipher:aes-speed-cipher enable formatter
    Signed-off-by: Abhiram S <abhiram.s@amd.com>

--- examples/cipher/aes-speed-cipher.c
+++ examples/cipher/aes-speed-cipher.c
@@ -63,28 +63,26 @@ printText(Uint8* I, Uint64 len, char* s)
 #endif
 }
 
-// clang-format off
-
-
 struct timeval begin, end;
-long   seconds;
-long   microseconds;
-double elapsed;
-double totalTimeElapsed;
+long           seconds;
+long           microseconds;
+double         elapsed;
+double         totalTimeElapsed;
 
 #if WIN32
-int gettimeofday(struct timeval* tv, struct timeval* tv1)
+int
+gettimeofday(struct timeval* tv, struct timeval* tv1)
 {
-    FILETIME    f_time;
-    Uint64    time;
-    SYSTEMTIME  s_time;
-    //define UNIX EPOCH time for windows
+    FILETIME   f_time;
+    Uint64     time;
+    SYSTEMTIME s_time;
+    // define UNIX EPOCH time for windows
     static const Uint64 EPOCH = ((Uint64)116444736000000000ULL);
     GetSystemTimeAsFileTime(&f_time);
     FileTimeToSystemTime(&f_time, &s_time);
     time = ((Uint64)f_time.dwLowDateTime);
     time += ((Uint64)f_time.dwHighDateTime) << 32;
-    tv->tv_sec = (long)((time - EPOCH) / 10000000L);
+    tv->tv_sec  = (long)((time - EPOCH) / 10000000L);
     tv->tv_usec = (long)(s_time.wMilliseconds * 1000);
     return 0;
 }
@@ -92,12 +90,13 @@ int gettimeofday(struct timeval* tv, struct timeval* tv1)
 
 #define ALCP_CRYPT_TIMER_START gettimeofday(&begin, 0);
 
-static inline void  alcp_get_time(int x, char *s)
+static inline void
+alcp_get_time(int x, char* s)
 {
     gettimeofday(&end, 0);
-    seconds = end.tv_sec - begin.tv_sec;
+    seconds      = end.tv_sec - begin.tv_sec;
     microseconds = end.tv_usec - begin.tv_usec;
-    elapsed = seconds + microseconds * 1e-6;
+    elapsed      = seconds + microseconds * 1e-6;
     totalTimeElapsed += elapsed;
     if (x) {
         printf("%s\t", s);
@@ -117,21 +116,20 @@ getinput(Uint8* output, int inputLen, int seed)
 }
 
 int
-create_aes_session(Uint8*             key,
-                   Uint8*             iv,
-                   const Uint32       key_len,
+create_aes_session(Uint8*                  key,
+                   Uint8*                  iv,
+                   const Uint32            key_len,
                    const alc_cipher_mode_t mode)
 {
     alc_error_t err;
     const int   err_size = 256;
-    Uint8     err_buf[err_size];
-    Uint8 tweakKey[16] = {
-    0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
-    0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xf, 0xf,
+    Uint8       err_buf[err_size];
+    Uint8       tweakKey[16] = {
+        0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
+        0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xf, 0xf,
     };
 
-
-    alc_cipher_info_t cinfo =     { // request params
+    alc_cipher_info_t cinfo = { // request params
                                 .ci_type   = ALC_CIPHER_TYPE_AES,
                                 .ci_mode   = mode,
                                 .ci_keyLen = key_len,
@@ -158,7 +156,6 @@ create_aes_session(Uint8*             key,
         goto out;
     }
 
-
     err = alcp_cipher_init(
         &handle, cinfo.ci_key, cinfo.ci_keyLen, cinfo.ci_iv, 16);
     if (alcp_is_error(err)) {
@@ -175,7 +172,9 @@ out:
     return -1;
 }
 
-void end_aes_session(){
+void
+end_aes_session()
+{
     /*
      * Complete the transaction
      */
@@ -192,7 +191,7 @@ aclp_aes_encrypt_demo(
 {
     alc_error_t err;
     const int   err_size = 256;
-    Uint8     err_buf[err_size];
+    Uint8       err_buf[err_size];
 
     err = alcp_cipher_encrypt(&handle, plaintxt, ciphertxt, len);
     if (alcp_is_error(err)) {
@@ -202,7 +201,6 @@ aclp_aes_encrypt_demo(
     }
 }
 
-
 void
 aclp_aes_decrypt_demo(
     const Uint8* ciphertxt,
@@ -211,7 +209,7 @@ aclp_aes_decrypt_demo(
 {
     alc_error_t err;
     const int   err_size = 256;
-    Uint8     err_buf[err_size];
+    Uint8       err_buf[err_size];
 
     err = alcp_cipher_decrypt(&handle, ciphertxt, plaintxt, len);
     if (alcp_is_error(err)) {
@@ -221,22 +219,21 @@ aclp_aes_decrypt_demo(
     }
 }
 
-
 /*
     Demo application for complete path:
     input->encrypt->cipher->decrypt->output.
     input and output is matched for comparison.
 */
 void
-encrypt_decrypt_demo(Uint8*       inputText,  // plaintext
-                     Uint32       inputLen,   // input length
-                     Uint8*       cipherText, // ciphertext output
+encrypt_decrypt_demo(Uint8*            inputText,  // plaintext
+                     Uint32            inputLen,   // input length
+                     Uint8*            cipherText, // ciphertext output
                      alc_cipher_mode_t m,
-                     int i)
+                     int               i)
 {
-    int retval = 0;
+    int          retval = 0;
     unsigned int keybits;
-    Uint8      key[32];
+    Uint8        key[32];
     int          ret = 0;
 
     memset(key, 0, 32);
@@ -249,10 +246,8 @@ encrypt_decrypt_demo(Uint8*       inputText,  // plaintext
     memset(iv, 10, 128 * 4);
 
     Uint8* ref;
-    ref = malloc(inputLen);
-    Uint32 ivLen  = 16;
-
-
+    ref          = malloc(inputLen);
+    Uint32 ivLen = 16;
 
     {
         keybits = 128 + i * 64;
@@ -267,15 +262,13 @@ encrypt_decrypt_demo(Uint8*       inputText,  // plaintext
         int seed = 1;
         getinput(inputText, inputLen, seed);
 
-
         memset(cipherText, 0, inputLen);
         memset(ref, 0, inputLen);
         memset(outputText, 0, inputLen);
         printText(key, 16, "key      ");
         printText(inputText, inputLen, "inputText");
 
-
-        //Encrypt speed test
+        // Encrypt speed test
         totalTimeElapsed = 0.0;
         retval           = create_aes_session(key, iv, keybits, m);
         for (int k = 0; k < 100000000; k++) {
@@ -301,7 +294,7 @@ encrypt_decrypt_demo(Uint8*       inputText,  // plaintext
         }
         end_aes_session();
 
-        //Decrypt speed test
+        // Decrypt speed test
         totalTimeElapsed = 0.0;
         retval           = create_aes_session(key, iv, keybits, m);
         for (int k = 0; k < 100000000; k++) {
@@ -309,13 +302,11 @@ encrypt_decrypt_demo(Uint8*       inputText,  // plaintext
             if (retval != 0)
                 goto out;
 
-
             ALCP_CRYPT_TIMER_START
             aclp_aes_decrypt_demo(
-                cipherText, // pointer to the PLAINTEXT
-                inputLen,   // text length in bytes
+                cipherText,  // pointer to the PLAINTEXT
+                inputLen,    // text length in bytes
                 outputText); // pointer to the CIPHERTEXT buffer
-
 
             alcp_get_time(0, "Decrypt time");
             printText(outputText, inputLen, "outputTxt");
@@ -332,7 +323,7 @@ encrypt_decrypt_demo(Uint8*       inputText,  // plaintext
         end_aes_session();
     }
 
-    out:
+out:
     if (outputText) {
         free(outputText);
     }
@@ -376,15 +367,16 @@ main(void)
             continue;
         }
 
-        // keep length multiple of 128bit (16x8)
-        #define MAX_TEST_CASE 7
-        int testblkSizes[MAX_TEST_CASE] = {16, 64, 256, 1024, 8192, 16384, 32768};
+// keep length multiple of 128bit (16x8)
+#define MAX_TEST_CASE 7
+        int testblkSizes[MAX_TEST_CASE] = { 16,   64,    256,  1024,
+                                            8192, 16384, 32768 };
 
-        for(int keySizeItr = 0; keySizeItr < 3; keySizeItr++) {
-            if((m == ALC_AES_MODE_XTS) &&(keySizeItr>0)) {
+        for (int keySizeItr = 0; keySizeItr < 3; keySizeItr++) {
+            if ((m == ALC_AES_MODE_XTS) && (keySizeItr > 0)) {
                 continue;
             }
-            for (int i =0; i < MAX_TEST_CASE; i++) {
+            for (int i = 0; i < MAX_TEST_CASE; i++) {
                 int inputLen = testblkSizes[i];
                 printf(" \n");
 
