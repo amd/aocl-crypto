f21c32095ec9f3b5746820a635babd72e6f2182a
ashanand <ashish.anand@amd.com>
2024-05-23 10:36:44 +0000

    Fixing the reference algorithm in RSA for non avx2 machine

--- lib/include/alcp/rsa/rsa_reference.hh
+++ lib/include/alcp/rsa/rsa_reference.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023, Advanced Micro Devices. All rights reserved.
+ * Copyright (C) 2023-2024, Advanced Micro Devices. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -30,7 +30,7 @@
 #pragma once
 
 #ifdef COMPILER_IS_GCC
-#define NO_OPTIMIZE
+#define NO_OPTIMIZE _Pragma("GCC optimize \"O0\"")
 #else
 #define NO_OPTIMIZE __attribute__((optnone))
 #endif
