d3b054c18776049766c95cb6e4a29886303e0efb
ashanand <ashish.anand@amd.com>
2024-06-10 14:50:50 +0000

    Fixing fuxx issues

--- tests/Fuzz/Rsa/test_fuzz_rsa_sign_verify.cc
+++ tests/Fuzz/Rsa/test_fuzz_rsa_sign_verify.cc
@@ -99,7 +99,7 @@ ALCP_Fuzz_Rsa_SignVerify(int PaddingMode, const Uint8* buf, size_t len)
         printf("Error: alcp_rsa_add_digest\n");
         goto dealloc;
     }
-    err = alcp_rsa_add_mgf(&handle, &mgf_info);
+    err = alcp_rsa_add_mgf(&handle, ALC_SHA2_256);
     if (alcp_is_error(err)) {
         printf("Error: alcp_rsa_add_mgf\n");
         goto dealloc;
