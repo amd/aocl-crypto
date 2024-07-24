4facabc039f6540418ea6f623c26cc61d92269d8
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2024-07-24 09:39:24 +0000

    Crypto: Coverity: ECDH IPP: Uninitialized class members
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>

--- tests/ecdh/base/ipp_ecdh.cc
+++ tests/ecdh/base/ipp_ecdh.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023, Advanced Micro Devices. All rights reserved.
+ * Copyright (C) 2023-2024, Advanced Micro Devices. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -62,7 +62,7 @@ IPPEcdhBase::GeneratePublicKey(const alcp_ecdh_data_t& data)
     /* load keys */
     /* TODO: when there is alcp multi-buffer implementation available, modify
      * this*/
-    Uint8 m_pPublicKeyData_mb_temp_buff[7][ECDH_KEYSIZE];
+    Uint8 m_pPublicKeyData_mb_temp_buff[7][ECDH_KEYSIZE]{};
 
     m_pPublicKeyData_mb[0] = data.m_Peer_PubKey;
 
